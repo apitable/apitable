0178378565c36cf92a46e1a9df305226073eb56b
JaneSu <sujian@vikadata.com>
2022-09-07 15:11:32 +0800

    feat: 彩虹标签使用支持硬限制

--- packages/datasheet/src/pc/common/billing/trigger_usage_alert.ts
+++ packages/datasheet/src/pc/common/billing/trigger_usage_alert.ts
@@ -3,18 +3,20 @@ import { showVikaby } from 'pc/common/guide/vikaby';
 import { usageWarnModal } from 'pc/components/subscribe_system/usage_warn_modal/usage_warn_modal';
 import { subscribeUsageCheck, SubscribeUsageTipType } from './subscribe_usage_check';
 
-export const triggerUsageAlert = (functionName: keyof ISubscription, extra?: Record<string, any>, tipType?: SubscribeUsageTipType) => {
+export const triggerUsageAlert = (functionName: keyof ISubscription, extra?: Record<string, any>, tipType?: SubscribeUsageTipType): boolean => {
   const result = subscribeUsageCheck.triggerVikabyAlert(functionName, extra);
 
   if (!result) {
-    return;
+    return false;
   }
 
   const { title, content } = result;
 
   if (tipType === SubscribeUsageTipType.Alert) {
-    return usageWarnModal({ alertContent: content });
+    usageWarnModal({ alertContent: content });
+    return true;
   }
+
   showVikaby({
     defaultExpandDialog: true,
     dialogConfig: {
@@ -23,4 +25,5 @@ export const triggerUsageAlert = (functionName: keyof ISubscription, extra?: Rec
       dialogClx: 'billingNotify',
     },
   });
+  return true;
 };
