d203bb935e52e01094a9a71c73fb245be9b37551
Alexander Zhao <zhaomengzhe@vikadata.com>
2022-09-19 16:34:03 +0800

    feat(init-settings): add custom_config.js intro comments

---
+++ packages/datasheet/public/custom/custom_config.js
@@ -0,0 +1,5 @@
+/**
+ * this custom_config.js is empty in dev environment
+ * during docker build, /settings/script.js will executed to inject custom configs here
+ * in order to replace the language packages and some public files with the editions ones
+ */
