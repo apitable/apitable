5561d183eac2af58c36e5830cdea0ef4c38e9e67
Aria <sujian@vikadata.com>
2022-11-03 18:21:29 +0800

    refactor: add sentry capture (#1692)

--- packages/datasheet/src/error_page.tsx
+++ packages/datasheet/src/error_page.tsx
@@ -6,7 +6,7 @@ import { Strings, t } from '@apitable/core';
 import { navigationToUrl } from 'pc/components/route_manager/navigation_to_url';
 import * as React from 'react';
 
-const ErrorPage: React.FC = () => {
+const ErrorPage = () => {
   const handleClick = () => {
     window.location.href = '/workbench';
   };
@@ -22,14 +22,14 @@ const ErrorPage: React.FC = () => {
   return (
     <div className={'errorPage'}>
       <div className={'errorText'}>
-        {t(Strings.error_boundary_crashed)}<span role="img" aria-label="sick">🤒</span>
+        {t(Strings.error_boundary_crashed)}<span role='img' aria-label='sick'>🤒</span>
       </div>
-      <Button color="primary" onClick={handleClick}>{t(Strings.error_boundary_back)}</Button>
+      <Button color='primary' onClick={handleClick}>{t(Strings.error_boundary_back)}</Button>
       <LinkButton underline={false} onClick={handleContactUs}>
-        <Typography className={'contactUs'} variant="body2" color={colorVars.fc0}>{t(Strings.contact_us)}</Typography>
+        <Typography className={'contactUs'} variant='body2' color={colorVars.fc0}>{t(Strings.contact_us)}</Typography>
       </LinkButton>
       <LinkButton className={'errorFeedback'} underline color={colorVars.fc3} onClick={handleFeedback}>
-        <Typography variant="body2" color={colorVars.fc3}>{t(Strings.error_page_feedback_text)}</Typography>
+        <Typography variant='body2' color={colorVars.fc3}>{t(Strings.error_page_feedback_text)}</Typography>
       </LinkButton>
     </div>
   );
