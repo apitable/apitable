6339496d4b9a6524d02965611e75523da51f06be
Aria <jianweisummer@gmail.com>
2023-01-03 15:41:30 +0800

    fix: incomplete loading of associated data in the mirror (#2709)
    Closes #2431
    
    
    
    Submit a pull request for this project.
    
    <!-- 如果这个Pull Request有对应的Issue，你可以复制Issue的相关信息。 -->
    
    # Why? 
    <!-- 
    > 对应哪个issue?
    > 为什么要这个pull request？
    > 背景故事或原因是怎样的？
    > 你理解的业务需求是怎样的？ 
    -->
    
    
    # What?
    <!-- 
    > 这是一个什么Pull Request？
    > 描述一下是什么？
    > 对哪些人有好处？ 
    -->
    
    
    # How?
    <!-- 
    > 大概描述下，如何具体实现的这个Pull Request？ 
    -->

--- packages/datasheet/src/pc/utils/get_datasheet_or_load.ts
+++ packages/datasheet/src/pc/utils/get_datasheet_or_load.ts
@@ -1,10 +1,10 @@
-import { Selectors, IReduxState, StoreActions } from '@apitable/core';
+import { IReduxState, Selectors, StoreActions } from '@apitable/core';
 import { store } from 'pc/store';
 
 export const getDatasheetOrLoad = (
-  state: IReduxState, foreignDatasheetId: string, dstId?: string, assignDstId?: boolean, forceFetch?: boolean, sourceMirrorId?: string
+  state: IReduxState, foreignDatasheetId: string, dstId?: string, assignDstId?: boolean, forceFetch?: boolean, ignoreMirror?: boolean
 ) => {
-  const { formId, mirrorId = sourceMirrorId } = state.pageParams;
+  const { formId, mirrorId } = state.pageParams;
   const datasheetId = dstId || state.pageParams.datasheetId;
   const datasheet = Selectors.getDatasheet(state, foreignDatasheetId);
   const datasheetLoading = Selectors.getDatasheetLoading(state, foreignDatasheetId);
@@ -19,7 +19,7 @@ export const getDatasheetOrLoad = (
       store.dispatch(StoreActions.fetchForeignDatasheet(formId, foreignDatasheetId, forceFetch));
       return null;
     }
-    if (mirrorId) {
+    if (mirrorId && !ignoreMirror) {
       store.dispatch(StoreActions.fetchForeignDatasheet(mirrorId, foreignDatasheetId, forceFetch));
       return null;
     }
