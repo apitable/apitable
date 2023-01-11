06f17607563beb72fea6baa11c7aa3ffb31b0d42
Troy <fengjun@vikadata.com>
2023-01-11 15:37:24 +0800

    refactor: remove useless codes

--- packages/room-server/src/developer/services/developer.service.spec.ts
+++ packages/room-server/src/developer/services/developer.service.spec.ts
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-// import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { Test, TestingModule } from '@nestjs/testing';
 import { DeveloperRepository } from '../repositories/developer.repository';
 import { UserRepository } from '../../user/repositories/user.repository';
