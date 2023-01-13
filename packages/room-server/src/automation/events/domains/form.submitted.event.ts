e4f8d1878dedfffa72c00d73b89fd3ef31fc67fa
Kelly Chan <kelly@apitable.com>
2023-01-13 22:08:08 +0800

    fix: eslint fix &  super-linter ready (#181)
    fix eslint

--- packages/room-server/src/automation/events/domains/form.submitted.event.ts
+++ packages/room-server/src/automation/events/domains/form.submitted.event.ts
@@ -1,13 +1,13 @@
-import { IEventInstance, IOPEvent } from "@apitable/core";
+import { IEventInstance, IOPEvent } from '@apitable/core';
 
 export type FormSubmittedEvent = Omit<IEventInstance<IOPEvent>, 'context'> & {
   context: FormSubmittedEventContext,
 } & {
   beforeApply: boolean,
-}
+};
 
 export type FormSubmittedEventContext = {
   formId: string,
   recordId: string,
   [key: string]: any,
-}
+};
