140a936d615141fde828bb89a43fe18d4af308b6
Troy <fengjun@vikadata.com>
2023-01-16 15:59:23 +0800

    chore: yarn add dev dependency typescript-eslint

---
+++ packages/room-server/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+};
