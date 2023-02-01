df2f3f9793760fc4b188085b33fbf04502c77da5
Troy <fengjun@vikadata.com>
2023-02-01 12:09:14 +0800

    test: mirror service fetchDataPack

--- packages/room-server/src/database/mirror/services/mirror.service.spec.ts
+++ packages/room-server/src/database/mirror/services/mirror.service.spec.ts
@@ -19,15 +19,20 @@
 import { IPermissions, IResourceMeta, Role } from '@apitable/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DatasheetMetaRepository } from 'database/datasheet/repositories/datasheet.meta.repository';
 import { DatasheetMetaService } from 'database/datasheet/services/datasheet.meta.service';
 import { DatasheetRecordService } from 'database/datasheet/services/datasheet.record.service';
+import { DatasheetService } from 'database/datasheet/services/datasheet.service';
 import { NodeInfo } from 'database/interfaces';
 import { ResourceMetaRepository } from 'database/resource/repositories/resource.meta.repository';
+import { WinstonModule } from 'nest-winston';
 import { NodeService } from 'node/services/node.service';
+import { ServerException, DatasheetException } from 'shared/exception';
+import { LoggerConfigService } from 'shared/services/config/logger.config.service';
 import { RestService } from 'shared/services/rest/rest.service';
 import { MirrorService } from './mirror.service';
 
-describe('DashboardService', () => {
+describe('MirrorService', () => {
   let app: NestFastifyApplication;
   let module: TestingModule;
   let service: MirrorService;
@@ -35,7 +40,10 @@ describe('DashboardService', () => {
   let datasheetMetaService: DatasheetMetaService;
   let datasheetRecordService: DatasheetRecordService;
   let resourceMetaRepository: ResourceMetaRepository;
+  let datasheetMetaRepository: DatasheetMetaRepository;
   const knownMirrorId = 'mirqyLwKo4ecDxrM7e';
+  const knownRecordId = 'recqedLfdfsad2KDda';
+  const relatedDatasheetId = 'dstlljhDwewfds32Dfds';
   const permissions: IPermissions = Object.assign({ allowEditConfigurable: false });
   const nodeInfo: NodeInfo = Object.assign({ id: knownMirrorId, name: 'Test Mirror', role: Role.Editor, nodeFavorite: false, permissions });
   const unknownMirrorId = `mir${Math.floor(Math.random()*10000).toString()}`;
@@ -77,13 +85,19 @@ describe('DashboardService', () => {
 
   beforeAll(async() => {
     module = await Test.createTestingModule({
-      imports: [],
+      imports: [
+        WinstonModule.forRootAsync({
+          useClass: LoggerConfigService,
+        }),
+      ],
       providers: [
         MirrorService,
+        DatasheetMetaService,
         { 
           provide: NodeService, 
           useValue: { 
-            getNodeDetailInfo: jest.fn() 
+            getNodeDetailInfo: jest.fn(),
+            getMainNodeId: jest.fn() 
           }
         }, 
         { 
@@ -93,7 +107,23 @@ describe('DashboardService', () => {
             fetchMe: jest.fn() 
           }
         }, 
-        ResourceMetaRepository
+        { 
+          provide: DatasheetService, 
+          useValue: { 
+            processField: jest.fn(()=>{
+              return {};
+            }), 
+          }
+        }, 
+        { 
+          provide: DatasheetRecordService, 
+          useValue: { 
+            getRecordsByDstId: jest.fn(), 
+            getRecordsByDstIdAndRecordIds: jest.fn() 
+          }
+        }, 
+        ResourceMetaRepository,
+        DatasheetMetaRepository
       ],
     }).compile();
     app = module.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
@@ -110,14 +140,40 @@ describe('DashboardService', () => {
     datasheetMetaService = module.get<DatasheetMetaService>(DatasheetMetaService);
     datasheetRecordService = module.get<DatasheetRecordService>(DatasheetRecordService);
     resourceMetaRepository = module.get<ResourceMetaRepository>(ResourceMetaRepository);
+    datasheetMetaRepository = module.get<DatasheetMetaRepository>(DatasheetMetaRepository);
     jest.spyOn(resourceMetaRepository, 'selectMetaByResourceId').mockImplementation(async(mirrorId) => {
       if (mirrorId === knownMirrorId) {
         return await Promise.resolve(meta);
       }
       return await Promise.resolve({});
     });
+    jest.spyOn(datasheetMetaRepository, 'selectMetaByDstId').mockImplementation(async(datasheetId: string) => {
+      if (datasheetId === relatedDatasheetId) {
+        return await Promise.resolve(Object.assign({ metaData: meta, revision: 1000 }));
+      }
+      return await Promise.resolve(undefined);
+    });
+    
     jest.spyOn(nodeService, 'getNodeDetailInfo').mockImplementation(async() => {
       return await Promise.resolve({ node: nodeInfo });
+    });
+    jest.spyOn(nodeService, 'getMainNodeId').mockImplementation(async(nodeId: string) => {
+      if (nodeId === knownMirrorId) {
+        return await Promise.resolve(relatedDatasheetId);
+      }
+      throw new ServerException(DatasheetException.DATASHEET_NOT_EXIST);
+    });
+    jest.spyOn(datasheetRecordService, 'getRecordsByDstIdAndRecordIds').mockImplementation(async(dstId: string, recordIds: string[]) => {
+      if (dstId === relatedDatasheetId && recordIds.includes(knownRecordId)) {
+        return await Promise.resolve({ knownRecordId: { id: knownRecordId, data: {}}});
+      }
+      return await Promise.resolve({});
+    });
+    jest.spyOn(datasheetRecordService, 'getRecordsByDstId').mockImplementation(async(dstId: string) => {
+      if (dstId === relatedDatasheetId) {
+        return await Promise.resolve({ knownRecordId: { id: knownRecordId, data: {}}});
+      }
+      return await Promise.resolve({});
     });
 
   });
@@ -136,19 +192,32 @@ describe('DashboardService', () => {
 
   describe('test fetchDataPack', () => {
 
-    it('should return empty widgetInstallations with an unknown/deleted dashboard ID', async() => {
-      const res = await service.fetchDataPack(unknownMirrorId, auth, { internal: false });
-      expect(res.snapshot.meta).toEqual({});
+    it('should throw error with an unknown/deleted mirror ID', async() => {
+      try {
+        await service.fetchDataPack(unknownMirrorId, auth, { internal: false });
+      } catch (error) {
+        expect((error as any).message).toEqual(DatasheetException.DATASHEET_NOT_EXIST.getMessage());
+      }
     });
 
-    it('should return widgetInstallations with an known dashboard ID', async() => {
+    it('should return empty record map with empty record IDs', async() => {
+      const res = await service.fetchDataPack(knownMirrorId, auth, { internal: false, recordIds: [] });
+      expect(res.snapshot.recordMap).toEqual({});
+    });
+
+    it('should return empty record map with unknown record IDs', async() => {
+      const res = await service.fetchDataPack(knownMirrorId, auth, { internal: false, recordIds: ['rec111111'] });
+      expect(res.snapshot.recordMap).toEqual({});
+    });
+
+    it('should return meta without record IDs', async() => {
       const res = await service.fetchDataPack(knownMirrorId, auth, { internal: false });
       expect(res.snapshot.meta).toEqual(meta);
     });
 
-    it('should return empty widgetMap without layout', async() => {
-      const res = await service.fetchDataPack(knownMirrorId, auth, { internal: false });
-      expect(res.snapshot.meta).toBeUndefined();
+    it('should return meta with known record ID', async() => {
+      const res = await service.fetchDataPack(knownMirrorId, auth, { internal: false, recordIds: [knownRecordId] });
+      expect(res.snapshot.meta).toEqual(meta);
     });
 
   });
