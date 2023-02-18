4cadce3d431315c54cdc90bdf17442942c18c754
Kelly Chan <kelly@apitable.com>
2023-02-18 22:37:38 +0800

    feat: APITable support to reset password (#401)
    Co-authored-by: plodsoft <xplzjwz@gmail.com>
    Co-authored-by: Zoe <zhengxu@apitable.com>
    Co-authored-by: liuyi <liuyi@vikadata.com>
    Co-authored-by: Aria <jianweisummer@gmail.com>
    Co-authored-by: 王开朗 <wangkailang@users.noreply.github.com>
    Co-authored-by: Bron ZiJing Liu <liuzijing@vikadata.com>
    Co-authored-by: Shawn Deng <dengguiheng@vikadata.com>

--- packages/datasheet/src/pc/components/navigation/account_center_modal/modify_password/modify_password.tsx
+++ packages/datasheet/src/pc/components/navigation/account_center_modal/modify_password/modify_password.tsx
@@ -183,7 +183,7 @@ export const ModifyPassword: FC<React.PropsWithChildren<IModifyPasswordProps>> =
       return null;
     });
   };
-
+  
   return (
     <div className={styles.modifyPasswordWrapper}>
       <div className={styles.title}>{user!.needPwd ? t(Strings.set_password) : t(Strings.change_password)}</div>
--- packages/datasheet/src/pc/components/widget/widget_center/index.ts
+++ packages/datasheet/src/pc/components/widget/widget_center/index.ts
@@ -17,3 +17,9 @@
  */
 
 export * from './widget_center';
+export { WidgetPackageItem } from './widget_package_item';
+export { InstallPosition } from './enum';
+export { installWidget } from './install_utils';
+export { installToDashboard } from './install_utils';
+export { installToPanel } from './install_utils';
+export { WidgetPackageList } from './widget_package_list';
