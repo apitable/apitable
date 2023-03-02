dc2b27f608db9ae2640d1f868b9400ac656fd7c8
Boris Bo Wang <wangbo@vikadata.com>
2023-03-02 18:48:44 +0800

    fix: widget initialize data without binding datasheet data (#3636)

--- packages/datasheet/src/pc/components/widget/widget_panel/widget_item/widget_block.tsx
+++ packages/datasheet/src/pc/components/widget/widget_panel/widget_item/widget_block.tsx
@@ -76,7 +76,8 @@ export const WidgetBlockBase: React.ForwardRefRenderFunction<IWidgetBlockRefs, {
   const iframeRef = useRef<HTMLIFrameElement>(null);
   const [codeUrl, setCodeUrl] = useCloudStorage<string | undefined>(`widget_loader_code_url_${widgetPackageId}`, widgetId);
   const nodeConnected = useSelector(state => {
-    const bindDatasheetLoaded = Boolean(Selectors.getDatasheetPack(state, nodeId));
+    const datasheet = Selectors.getDatasheet(state, nodeId);
+    const bindDatasheetLoaded = datasheet && !datasheet.isPartOfData;
     // The initialization of the widget must be done after the datasheet loaded.
     if (!bindDatasheetLoaded) {
       return false;
