500828b7e741377c677c7888c8434a6c9c1fc068
Boris Bo Wang <wangbo@vikadata.com>
2023-03-10 16:27:56 +0800

    feat: quick search (#3787)

--- packages/datasheet/src/pc/components/quick_search/index.ts
+++ packages/datasheet/src/pc/components/quick_search/index.ts
@@ -16,4 +16,4 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-export * from './search';
+export * from './quick_search';
--- packages/icons/src/components/loading_outlined.tsx
+++ packages/icons/src/components/loading_outlined.tsx
@@ -21,7 +21,7 @@ import React from 'react';
 import { makeIcon, IIconProps } from '../utils/icon';
 
 export const LoadingOutlined: React.FC<IIconProps> = makeIcon({
-    Path: ({ colors }) => <>
+  Path: ({ colors }) => <>
     <path d="M8 1C7.58579 1 7.25 1.33579 7.25 1.75C7.25 2.16421 7.58579 2.5 8 2.5C11.0376 2.5 13.5 4.96243 13.5 8C13.5 8.41421 13.8358 8.75 14.25 8.75C14.6642 8.75 15 8.41421 15 8C15 4.13401 11.866 1 8 1Z" fill={ colors[0] }/>
 
   </>,
