028e664d5bf4db6f80080e5d89a8e19787f81bb9
SkyHuang <huangchangpeng@vikadata.com>
2023-03-29 16:04:36 +0800

    fix: components style (#4201)
    - #4123 
    - #4126

--- packages/datasheet/src/pc/components/record_picker/search_content/row.tsx
+++ packages/datasheet/src/pc/components/record_picker/search_content/row.tsx
@@ -1,5 +1,4 @@
 import React, { memo } from 'react';
-import { useTheme } from '@apitable/components';
 import { ListChildComponentProps } from 'react-window';
 import { RecordCard } from './record_card';
 import styles from './style.module.less';
@@ -7,20 +6,17 @@ import classNames from 'classnames';
 
 export const Row: React.FC<ListChildComponentProps> = memo(props => {
   const { index, data, style } = props;
-  const { color } = useTheme();
 
   const {
     rows,
     columns,
     fieldMap,
-    focusIndex,
     selectedSet,
     datasheetId,
     onClick,
   } = data;
   const row = rows[index];
   const recordId = row.recordId;
-  const isFocus = focusIndex === index;
   const isSelected = selectedSet.has(recordId);
 
   return (
@@ -38,9 +34,6 @@ export const Row: React.FC<ListChildComponentProps> = memo(props => {
           columns={columns}
           fieldMap={fieldMap}
           datasheetId={datasheetId}
-          style={{ 
-            backgroundColor: isFocus ? color.bgWarnLightHover : color.fill0
-          }}
           onClick={onClick}
         />
       </div>
