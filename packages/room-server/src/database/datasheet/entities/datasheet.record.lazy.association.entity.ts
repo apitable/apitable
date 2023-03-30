60883d08407f2d9315e03f643de74b10b9e04cba
Troy <fengjun@vikadata.com>
2023-03-31 00:01:34 +0800

    feat: record lazy association service, entity and repository

---
+++ packages/room-server/src/database/datasheet/entities/datasheet.record.lazy.association.entity.ts
@@ -0,0 +1,82 @@
+/**
+ * APITable <https://github.com/apitable/apitable>
+ * Copyright (C) 2022 APITable Ltd. <https://apitable.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { IRecordDependencies, IRecordMeta } from '@apitable/core';
+import { IdWorker } from 'shared/helpers';
+import { BeforeInsert, Column, Entity, PrimaryColumn } from 'typeorm';
+
+/**
+ * datasheet record lazy association
+ */
+@Entity('datasheet_record_lazy_association')
+export class DatasheetRecordLazyAssociationEntity {
+
+  @PrimaryColumn('bigint')
+  id!: string;
+
+  @Column({
+    name: 'space_id',
+    nullable: true,
+    comment: 'space ID',
+    length: 50,
+  })
+  spaceId!: string;
+
+  @Column({
+    name: 'dst_id',
+    nullable: true,
+    comment: 'datasheet ID',
+    length: 50,
+  })
+  dstId!: string;
+
+  @Column({
+    name: 'record_id',
+    nullable: true,
+    comment: 'record ID',
+    length: 50,
+  })
+  recordId!: string;
+
+  @Column('json', {
+    name: 'depends',
+    nullable: true,
+    comment: 'record dependencies(related datasheets\' records and fields)',
+  })
+  depends?: IRecordDependencies;
+
+  @Column('timestamp', {
+    name: 'created_at',
+    comment: 'created time',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt!: Date;
+
+  @Column('timestamp', {
+    name: 'updated_at',
+    nullable: true,
+    comment: 'updated time',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt?: Date;
+
+  @BeforeInsert()
+  beforeInsert() {
+    this.id = IdWorker.nextId().toString();
+  }
+}
---
+++ packages/room-server/src/database/datasheet/services/datasheet.record.lazy.association.service.ts
@@ -0,0 +1,78 @@
+/**
+ * APITable <https://github.com/apitable/apitable>
+ * Copyright (C) 2022 APITable Ltd. <https://apitable.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Injectable } from '@nestjs/common';
+import { DatasheetRecordLazyAssociationEntity } from '../entities/datasheet.record.lazy.association.entity';
+import { DatasheetRecordLazyAssociationRepository } from '../repositories/datasheet.record.lazy.association.repository';
+
+@Injectable()
+export class DatasheetRecordLazyAssociationService {
+
+  constructor(
+    private readonly repository: DatasheetRecordLazyAssociationRepository,
+  ) {}
+
+  public async getRecordAssociations(spaceId: string, datasheetId: string, recordIds: string[]): Promise<DatasheetRecordLazyAssociationEntity[]> {
+    const associations = await this.repository.find({
+      where: {
+        spaceId,
+        datasheetId,
+        recordId: recordIds,
+      },
+    });
+    return associations;
+  }
+
+  public async insertOrUpdateRecordAssociations(associations: DatasheetRecordLazyAssociationEntity[]): Promise<void> {
+    await this.repository.save(associations, { transaction: true });
+  }
+
+  /**
+   *
+   * @param associations 
+   * @param existingAssociations 
+   * @returns 
+   * 
+   */
+  public diffRecordAssociations(associations: DatasheetRecordLazyAssociationEntity[]
+    , existingAssociations: DatasheetRecordLazyAssociationEntity[]): DatasheetRecordLazyAssociationEntity[] {
+    const associationsMap = new Map<string, DatasheetRecordLazyAssociationEntity>();
+    for (const association of associations) {
+      associationsMap.set(`${association.spaceId}-${association.dstId}-${association.recordId}`, association);
+    }
+    const existingAssociationsMap = new Map<string, DatasheetRecordLazyAssociationEntity>();
+    for (const association of existingAssociations) {
+      existingAssociationsMap.set(`${association.spaceId}-${association.dstId}-${association.recordId}`, association);
+    }
+    const associationsUpdated = [];
+    for (const association of associations) {
+      const key = `${association.spaceId}-${association.dstId}-${association.recordId}`;
+      if (!existingAssociationsMap.has(key)) {
+        associationsUpdated.push(association);
+      } else {
+        const existingAssociation = existingAssociationsMap.get(key)?.depends;
+        if (JSON.stringify(existingAssociation.depends) === JSON.stringify(association.depends)) {
+          existingAssociation.depends = association.depends;
+          associationsUpdated.push(existingAssociation);
+        }
+      }
+    }
+    return associationsUpdated;
+  }
+
+}
