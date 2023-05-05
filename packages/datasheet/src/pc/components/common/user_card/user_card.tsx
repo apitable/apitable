fb8edf24519be17549023594db63e9a2cc19762f
liuyi <liuyi@vikadata.com>
2023-05-05 18:52:06 +0800

    Fix/user info card UI (#4861)

--- packages/datasheet/src/pc/components/common/user_card/user_card.tsx
+++ packages/datasheet/src/pc/components/common/user_card/user_card.tsx
@@ -174,14 +174,14 @@ export const UserCard: FC<React.PropsWithChildren<IUserCard>> = ({
                 <Avatar
                   id={memberInfo?.memberId || userId || '0'}
                   src={memberInfo?.avatar || spareSrc}
-                  title={memberInfo?.nickName || memberInfo?.memberName || spareName || ''}
+                  title={memberInfo?.memberName || memberInfo?.nickName || spareName || ''}
                   avatarColor={memberInfo?.avatarColor}
                   {...avatarProps}
                   size={40}
                 />
                 <div className={styles.nameWrapper}>
                   <h6>
-                    <span>{ spareName || title }</span>
+                    <span>{ title || spareName }</span>
                     {permissionVisible && memberRole &&
                       <Tag className={styles.permissionWrapper} color={TagColors[memberRole]}>{ConfigConstant.permissionText[memberRole]}</Tag>
                     }
