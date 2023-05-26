244342aa9e40a04e651b39d929d079e5e440a51a
ziqiang <zhaozhiqiang@vikadata.com>
2023-05-26 11:52:13 +0800

    Chore: refactor make settings command (#5235)
    Submit a pull request for this project.
    
    <!-- 如果这个Pull Request有对应的Issue，你可以复制Issue的相关信息。 -->
    
    # Why? 
    <!-- 
    > 对应哪个issue?
    > 为什么要这个pull request？
    > 背景故事或原因是怎样的？
    > 你理解的业务需求是怎样的？ 
    -->
    过去的 `make l10n-settings` 命令过于复杂，且会产生过多需要commit
    的文件，让开发者不敢下手，现在使其简便，对文件作出变更之后，执行`make settings` 即可。
    
    ISSUE： https://github.com/vikadata/vikadata/issues/5209
    
    # What?
    <!-- 
    > 这是一个什么Pull Request？
    > 描述一下是什么？
    > 对哪些人有好处？ 
    -->
    
    
    # How?
    <!-- 
    > 大概描述下，如何具体实现的这个Pull Request？ 
    -->
    
    ---------
    
    Co-authored-by: Kelly Peilin Chan <kelly@vikadata.com>

--- scripts/language-generate.sh
+++ scripts/language-generate.sh
@@ -8,18 +8,7 @@ COPY_PATH="$5"
 APITABLE_BASE_DIR="$6"
 L10N_EDITION_DIR="$7"
 
-# language file
-if [ -e "${COPY_PATH}/config/strings.json" ]; then
-    echo "language file exists"
-else
-    ts-node "${L10N_SCRIPT_DIR}/mergeLanguageFile.ts" "strings" "${L10N_BASE_DIR}/language.manifest.json" "json" "${L10N_GEN_DIR}" "${L10N_BASE_DIR}" "$L10N_EDITION_DIR"
-    cp "${L10N_GEN_DIR}"/strings.*-*.json "${COPY_PATH}/config/"
-    cp "${L10N_GEN_DIR}"/strings.json "${COPY_PATH}/config/"
-    echo "successfully generate language json file"
-    if [ "${IS_COPY_TS}" = "y" ]; then
-        cp "${L10N_GEN_DIR}/stringkeys.interface.ts" "${APITABLE_BASE_DIR}packages/core/src/config/stringkeys.interface.ts"
-        echo "successfully generate stringkeys.interface.ts"
-    else
-        echo "not find the file ${L10N_GEN_DIR}/stringkeys.interface.ts"
-    fi
-fi
+ts-node "${L10N_SCRIPT_DIR}/mergeLanguageFile.ts" "strings" "json" "${L10N_GEN_DIR}" "${L10N_BASE_DIR}" "$L10N_EDITION_DIR"
+cp "${L10N_GEN_DIR}"/strings.*-*.json "${COPY_PATH}/config/"
+cp "${L10N_GEN_DIR}"/strings.json "${COPY_PATH}/config/"
+cp "${L10N_GEN_DIR}/stringkeys.interface.ts" "${APITABLE_BASE_DIR}packages/core/src/config/stringkeys.interface.ts"
