2bdcb2224984ec063b6db71622a675e9aa83e6a7
Aria <sujian@apitable.com>
2023-06-08 19:11:40 +0800

    Feat/copy widget (#5478)
    支持镜像，数表和仪表盘复制小程序

---
+++ packages/datasheet/src/pc/components/dashboard_panel/utils.ts
@@ -0,0 +1,36 @@
+import { copyWidget, installToDashboard } from '../widget/widget_center/install_utils';
+import { StoreActions } from '@apitable/core';
+import { batchActions } from 'redux-batched-actions';
+import { installedWidgetHandle } from '../widget/widget_panel/widget_panel_header';
+import { store } from '../../store';
+
+export const createWidgetByExistWidgetId = (widgetId: string, dashboardId: string) => {
+  return new Promise(async(resolve, reject) => {
+    let widgets;
+
+    try {
+      widgets = await copyWidget(widgetId, dashboardId!);
+
+      if (!widgets.length) {
+        reject();
+        return;
+      }
+      await installToDashboard(widgets[0], dashboardId!);
+    } catch (e: any) {
+      reject(e);
+      return;
+    }
+
+    const _batchActions: any[] = [];
+
+    widgets.forEach((item: any) => {
+      _batchActions.push(StoreActions.receiveInstallationWidget(item.id, item));
+    });
+
+    store.dispatch(batchActions(_batchActions));
+
+    installedWidgetHandle(widgets[widgets.length - 1].id);
+
+    resolve(null);
+  });
+};
