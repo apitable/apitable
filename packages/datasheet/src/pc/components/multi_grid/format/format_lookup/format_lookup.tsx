479e3e6d083132ade3a5369bdb3ce12cf86c656e
liuyi <liuyi@vikadata.com>
2023-06-19 15:25:39 +0800

    fix: sortInfo save error (#5670)

--- packages/datasheet/src/pc/components/multi_grid/format/format_lookup/format_lookup.tsx
+++ packages/datasheet/src/pc/components/multi_grid/format/format_lookup/format_lookup.tsx
@@ -227,7 +227,8 @@ export const FormateLookUp: React.FC<React.PropsWithChildren<IFormateLookUpProps
         });
       } else if(propertyKey === 'sortInfo' && openFilter) {
         updateField({
-          filterInfo: filterInfo,
+          filterInfo: filterInfo.conditions.length > 0 ? filterInfo : undefined,
+          sortInfo: value.rules.length > 0 ? value : undefined,
         });
       } else {
         updateField();
