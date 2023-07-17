1742e7a914ebc3830c9fcb5bcd17011336cc5eb1
Chambers <chenbochao@vikadata.com>
2023-07-17 16:56:14 +0800

    refactor: redefine automation api url (#6130)
    1:1 对齐数据库
    <!-- This is an auto-generated comment: release notes by openai -->
    ### Summary by OpenAI
    
    Release Notes:
    
    - New Feature: Added new routes for automation actions and action types
    in the NestJS application.
    - New Feature: Added a new route path `nest/v1/automation/robots` to the
    `RobotController` class.
    - Refactor: Redefined the API URL for the `RobotRunHistoryController` by
    adding a new route.
    - Refactor: Renamed controller routes for triggers and trigger types.
    
    > "Paths transformed, routes reborn,
    > Automation dances, from dusk till morn.
    > Bugs squashed, features shine,
    > A symphony of code, harmonious and fine."
    <!-- end of auto-generated comment: release notes by openai -->

--- packages/room-server/src/automation/controller/action.type.controller.ts
+++ packages/room-server/src/automation/controller/action.type.controller.ts
@@ -19,7 +19,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { RobotActionTypeBaseService } from '../services/robot.action.type.base.service';
 
-@Controller('nest/v1/robots/action-types')
+@Controller(['nest/v1/robots/action-types', 'nest/v1/automation/action-types'])
 export class RobotActionTypeController {
   constructor(
     private readonly robotActionTypeService: RobotActionTypeBaseService,
--- packages/room-server/src/automation/controller/trigger.type.controller.ts
+++ packages/room-server/src/automation/controller/trigger.type.controller.ts
@@ -19,7 +19,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { RobotTriggerTypeService } from '../services/robot.trigger.type.service';
 
-@Controller('nest/v1/robots/trigger-types')
+@Controller(['nest/v1/robots/trigger-types', 'nest/v1/automation/trigger-types'])
 export class RobotTriggerTypeController {
   constructor(
     private readonly robotTriggerTypeService: RobotTriggerTypeService,
