8ec2c2b10a049c3ea6535a997606a03a4772a890
liuyi <liuyi@vikadata.com>
2023-07-24 17:43:35 +0800

    Feat/live chat (#6337)

--- packages/datasheet/src/pc/hooks/use_contact_us.ts
+++ packages/datasheet/src/pc/hooks/use_contact_us.ts
@@ -16,28 +16,19 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { ConfigConstant } from '@apitable/core';
+// import { ConfigConstant } from '@apitable/core';
 // @ts-ignore
-import { clearWizardsData } from 'enterprise';
-import { TriggerCommands } from 'modules/shared/apphook/trigger_commands';
 import { navigationToUrl } from 'pc/components/route_manager/navigation_to_url';
 import { getEnvVariables } from 'pc/utils/env';
 import { useCallback } from 'react';
 
 export const useContactUs = () => {
   return useCallback(() => {
-    const { HELP_MENU_CONTACT_US_URL, HELP_MENU_CONTACT_US_TYPE } = getEnvVariables();
+    const { HELP_MENU_CONTACT_US_URL } = getEnvVariables();
 
     if (HELP_MENU_CONTACT_US_URL) {
       navigationToUrl(HELP_MENU_CONTACT_US_URL);
     }
-
-    if (HELP_MENU_CONTACT_US_TYPE !== 'qrcode') {
-      window.LiveChatWidget?.call('maximize');
-    } else {
-      clearWizardsData?.();
-      localStorage.setItem('vika_guide_start', 'vikaby');
-      TriggerCommands.open_guide_wizard?.(ConfigConstant.WizardIdConstant.CONTACT_US_GUIDE);
-    }
+    window.LiveChatWidget?.call('maximize');
   }, []);
 };
