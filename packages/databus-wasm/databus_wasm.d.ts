afdab46fdef2756f8b4aeb36d5b5e0d4e78d61e1
ziqiang <zhaozhiqiang@vikadata.com>
2023-08-06 17:13:26 +0800

    feat: wasm cache by indexDB (#6506)
    ## feat:
    - `wasm` 调用 `room-server` 的接口, 添加了新的参数 `room_server_url` 
    
    https://github.com/vikadata/vikadata/blob/68f505713cc692d26ac003841070b9d36b00ce6a/databus/databus-wasm/src/lib.rs#L89-L93
    - dataPack 接口通过 `wasm` 发起。
    
    ![image](https://github.com/vikadata/vikadata/assets/18455767/6ca5c2b6-1b6c-40a3-a301-ad309906dff2)
    - inedxDB 缓存 `dataPack` 内容
    
    ![image](https://github.com/vikadata/vikadata/assets/18455767/f28e4df8-67be-4b58-ae4d-f55fe8f434e4)
    - `room-server` 添加获取 `reversion` 的接口
    
    https://github.com/vikadata/vikadata/blob/7ae8a451c66c7786ca66cd6a2454c5876d721586/apitable/packages/room-server/src/database/datasheet/controllers/datasheet.controller.ts#L110-L113
    
    ## refactor:
    - `wasm-pack` 以 `--target bundler` 打包时无法用于 web 环境, 改为使用 `--target web`
    的打包结果
    
    https://github.com/vikadata/vikadata/blob/68f505713cc692d26ac003841070b9d36b00ce6a/databus/databus-wasm/Makefile#L25-L27
      - 前端 wasm 的启用开关
    前端可以通过 `window.__initialization_data__.envVars`
    获取到配置的环境变量，一般开关的启用通过这里的变量。不要存到 `localStorage` 里。
    
    ![image](https://github.com/vikadata/vikadata/assets/18455767/104ee017-f6af-4ec1-bc48-e645fbb4a47f)
      添加了三个环境变量用来配置 `wasm` 相关。
    
    https://github.com/vikadata/vikadata/blob/7ae8a451c66c7786ca66cd6a2454c5876d721586/apitable/packages/datasheet/src/get_env.ts#L25-L27
    
    ## 其它
    1. `wasm` 一样运行在浏览器里，可以自动携带 `cookie`, 所以 `wasm` 对接口的调用不需要做鉴权。
    2. 目前是缓存了一个完整的接口字符串，如果能把每次变更产生的新 `snapshot` 缓存起来，性能会更好，但是要做太多东西了。
    3. 有的表会多调用一个接口（下面的接口）获取关联表的`snapshot` 信息，这个应该也是可以缓存的，需要看一下调用这个接口的原因。
    
    https://github.com/vikadata/vikadata/blob/7ae8a451c66c7786ca66cd6a2454c5876d721586/apitable/packages/core/src/modules/database/api/datasheet_api.ts#L93-L101
    
    ---------
    
    Co-authored-by: Kelly Peilin Chan <kelly@apitable.com>

--- packages/databus-wasm/databus_wasm.d.ts
+++ packages/databus-wasm/databus_wasm.d.ts
@@ -7,10 +7,10 @@
 */
 export function add_tn(a: number, b: number): number;
 /**
-* @param {string} some_th
+* @param {string} _some_th
 * @returns {Promise<any>}
 */
-export function ping_2(some_th: string): Promise<any>;
+export function ping_2(_some_th: string): Promise<any>;
 /**
 * @param {string} _dst_id
 * @returns {any}
@@ -22,8 +22,13 @@ export class DataBusBridge {
   free(): void;
 /**
 * @param {string} base_url
+* @param {string} room_server_url
 */
-  constructor(base_url: string);
+  constructor(base_url: string, room_server_url: string);
+/**
+* @returns {Promise<void>}
+*/
+  init(): Promise<void>;
 /**
 */
   print(): void;
@@ -39,7 +44,8 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly __wbg_databusbridge_free: (a: number) => void;
-  readonly databusbridge_new: (a: number, b: number) => number;
+  readonly databusbridge_new: (a: number, b: number, c: number, d: number) => number;
+  readonly databusbridge_init: (a: number) => number;
   readonly databusbridge_print: (a: number) => void;
   readonly databusbridge_get_datasheet_pack: (a: number, b: number, c: number) => number;
   readonly ping_2: (a: number, b: number) => number;
@@ -48,7 +54,10 @@ export interface InitOutput {
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0b19592273112ffa: (a: number, b: number, c: number, d: number) => void;
   readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf88c8e3624339deb: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
   readonly __wbindgen_exn_store: (a: number) => void;
   readonly wasm_bindgen__convert__closures__invoke2_mut__h6f2d41a93fab5bb6: (a: number, b: number, c: number, d: number) => void;
 }
--- packages/room-server/src/database/datasheet/controllers/datasheet.controller.ts
+++ packages/room-server/src/database/datasheet/controllers/datasheet.controller.ts
@@ -107,6 +107,11 @@ export class DatasheetController {
     return { revision, recordMap };
   }
 
+  @Get('datasheets/:dstId/revision')
+  async getDataSheetRevision(@Param('dstId') dstId: string): Promise<number | undefined> {
+    return await this.resourceMetaService.getRevisionByDstId(dstId) ;
+  }
+
   @Get('datasheets/:dstId/views/:viewId/dataPack')
   async getViewPack(@Headers('cookie') cookie: string, @Param('dstId') dstId: string, @Param('viewId') viewId: string): Promise<ViewPack> {
     // check if the user belongs to this space
