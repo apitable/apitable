2da2fb7bfef70b9451a5361ddc7cc33d53497fb7
Aria <sujian@apitable.com>
2023-08-09 10:13:12 +0800

    chore: add tailwindcss (#6672)
    增加 tailwindcss 配置

---
+++ packages/datasheet/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
