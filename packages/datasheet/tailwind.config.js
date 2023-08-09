6d5c03225f9c812fab4a068c3a3af5af15dc9c79
Harry Yep <git@hode.co.uk>
2023-08-09 14:06:05 +0800

    feat: add tailwindcss-related config & plugin (#6684)
    **tailwindcss-related issue:
    https://github.com/vikadata/vikadata/issues/6671**
    
    # Why? 
    
    Better formatting and fixed styles are standardized for best performance
    and usability when using tailwindcss.
    
    ![Demo for
    plugin](https://github.com/vikadata/vikadata/assets/66008528/3bc1e883-b3cc-474a-85c0-288857dedac7)
    
    
    # What?
    
    * darkMode: https://tailwindcss.com/docs/dark-mode
    * prettier-plugin-tailwindcss:
    https://tailwindcss.com/blog/automatic-class-sorting-with-prettier,
    https://github.com/tailwindlabs/prettier-plugin-tailwindcss
    
    # How?
    
    * add darkMode config to tailwindcss.config.js
    * add prettier-plugin-tailwindcss
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    **New Feature:**
    - Added `darkMode` configuration option in tailwindcss to support dark
    mode styling.
    - Expanded the `content` array in tailwindcss configuration to include
    additional file paths for processing.
    
    > 🌙 When the sun sets and the moon takes flight,  
    > Our app now shines with a new light.  
    > Dark mode is here, it's quite a sight,  
    > Tailwindcss makes it just right! 🎉  
    
    > 📁 More files join our merry band,  
    > In the content array, they now stand.  
    > Processed by tailwind, hand in hand,  
    > Making our app look grand! 🚀
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

--- packages/datasheet/tailwind.config.js
+++ packages/datasheet/tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  darkMode: ['class'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
