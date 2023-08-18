1d691ad98c7aa2fe75bb9b9eb25b30d4f8ba6298
yechen <18412022+SmileChen518@users.noreply.github.com>
2023-08-18 18:18:22 +0800

    feat: 第二十次提交rust替换“addRecord2Action”(Only one)函数(#6623)
    feat: 第二十次提交rust替换“addRecord2Action”(Only one)函数(#6623)
    site目录以及__test__测试都通过了

---
+++ packages/core/src/commands_actions/__test__/mock_data/action_add_record_mock.ts
@@ -0,0 +1,107 @@
+const MockDataForAddRecordAction = {
+  meta: {
+    views: [
+      {
+        id: 'viwzmCdQt493r',
+        name: 'Grid view',
+        rows: [
+          {
+            recordId: 'rec1j8vKtwrc5'
+          },
+          {
+            recordId: 'rec6iVgaz41fw'
+          },
+          {
+            recordId: 'recQ8h7BkoZxl'
+          }
+        ],
+        type: 1,
+        columns: [
+          {
+            fieldId: 'fldfMyqnl2N1p',
+            statType: 1
+          },
+          {
+            fieldId: 'fldyGQJZSEpQt'
+          },
+          {
+            fieldId: 'fldMu5bJRk9tf'
+          }
+        ],
+        autoSave: false,
+        frozenColumnCount: 1,
+        displayHiddenColumnWithinMirror: false
+      }
+    ],
+    fieldMap: {
+      fldMu5bJRk9tf: {
+        id: 'fldMu5bJRk9tf',
+        name: 'Attachments',
+        type: 6
+      },
+      fldfMyqnl2N1p: {
+        id: 'fldfMyqnl2N1p',
+        name: 'Title',
+        type: 19,
+        property: {
+          defaultValue: ''
+        }
+      },
+      fldyGQJZSEpQt: {
+        id: 'fldyGQJZSEpQt',
+        name: 'Options',
+        type: 4,
+        property: {
+          options: []
+        }
+      }
+    }
+  },
+  recordMap: {
+    rec1j8vKtwrc5: {
+      id: 'rec1j8vKtwrc5',
+      data: {},
+      createdAt: 1691821108000,
+      updatedAt: 1691821108000,
+      revisionHistory: [
+        0
+      ],
+      recordMeta: {
+        createdAt: 1691821108904,
+        createdBy: '8fcd23f82ec74d09915f7939845a8401'
+      },
+      commentCount: 0
+    },
+    rec6iVgaz41fw: {
+      id: 'rec6iVgaz41fw',
+      data: {},
+      createdAt: 1691821108000,
+      updatedAt: 1691821108000,
+      revisionHistory: [
+        0
+      ],
+      recordMeta: {
+        createdAt: 1691821108904,
+        createdBy: '8fcd23f82ec74d09915f7939845a8401'
+      },
+      commentCount: 0
+    },
+    recQ8h7BkoZxl: {
+      id: 'recQ8h7BkoZxl',
+      data: {},
+      createdAt: 1691821108000,
+      updatedAt: 1691821108000,
+      revisionHistory: [
+        0
+      ],
+      recordMeta: {
+        createdAt: 1691821108904,
+        createdBy: '8fcd23f82ec74d09915f7939845a8401'
+      },
+      commentCount: 0
+    }
+  },
+  datasheetId: 'dst5rANU3FyLWZ6TjQ'
+};
+
+export default MockDataForAddRecordAction;
