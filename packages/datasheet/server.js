cecfa9a39c52bf5f8da76e84dbbfdc1d321118d1
Kelly Peilin Chan <kelly@apitable.com>
2023-08-22 14:18:49 +0800

    Revert "fix(databus): ai model field: model"
    This reverts commit 3f4c42fbdc3f4962127405e774725391490d6c99.

--- packages/datasheet/server.js
+++ packages/datasheet/server.js
@@ -23,63 +23,60 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 const app = next({ dev: isDevelopment, port, hostname: 'localhost' });
 const handle = app.getRequestHandler();
 
-console.log('PWD:' + process.cwd());
+app.prepare().then(() => {
+  const server = express();
 
-app.prepare();
-// app.prepare().then(() => {
-//   const server = express();
+  if (isDevelopment) {
+    server.use(createProxyMiddleware('/databus', {
+              target: process.env.API_PROXY || 'http://127.0.0.1:8082',
+              changeOrigin: true,
+              cookieDomainRewrite: '',
+          })
+    );
 
-//   if (isDevelopment) {
-//     server.use(createProxyMiddleware('/databus', {
-//               target: process.env.API_PROXY || 'http://127.0.0.1:8082',
-//               changeOrigin: true,
-//               cookieDomainRewrite: '',
-//           })
-//     );
+    server.use(createProxyMiddleware('/nest', {
+        // Direct connection to local NodeJS environment
+        target: process.env.API_PROXY || process.env.API_ROOM_SERVER || 'http://127.0.0.1:3333',
+        changeOrigin: true,
+        cookieDomainRewrite: '',
+      })
+    );
 
-//     server.use(createProxyMiddleware('/nest', {
-//         // Direct connection to local NodeJS environment
-//         target: process.env.API_PROXY || process.env.API_ROOM_SERVER || 'http://127.0.0.1:3333',
-//         changeOrigin: true,
-//         cookieDomainRewrite: '',
-//       })
-//     );
+    server.use(createProxyMiddleware('/api', {
+        target: process.env.API_PROXY || process.env.API_BACKEND_SERVER || 'http://127.0.0.1:8081',
+        changeOrigin: true,
+        cookieDomainRewrite: '',
+      })
+    );
 
-//     server.use(createProxyMiddleware('/api', {
-//         target: process.env.API_PROXY || process.env.API_BACKEND_SERVER || 'http://127.0.0.1:8081',
-//         changeOrigin: true,
-//         cookieDomainRewrite: '',
-//       })
-//     );
+    server.use(createProxyMiddleware('/fusion', {
+        target: process.env.API_PROXY || process.env.API_FUSION_SERVER || 'http://127.0.0.1',
+        changeOrigin: true,
+        cookieDomainRewrite: '',
+      })
+    );
 
-//     server.use(createProxyMiddleware('/fusion', {
-//         target: process.env.API_PROXY || process.env.API_FUSION_SERVER || 'http://127.0.0.1',
-//         changeOrigin: true,
-//         cookieDomainRewrite: '',
-//       })
-//     );
+    server.use(createProxyMiddleware('/room', {
+      target: process.env.API_PROXY || process.env.API_SOCKET_SERVER_ROOM || 'http://127.0.0.1:3005',
+      ws: true,
+      changeOrigin: true,
+      cookieDomainRewrite: ''
+    }));
 
-//     server.use(createProxyMiddleware('/room', {
-//       target: process.env.API_PROXY || process.env.API_SOCKET_SERVER_ROOM || 'http://127.0.0.1:3005',
-//       ws: true,
-//       changeOrigin: true,
-//       cookieDomainRewrite: ''
-//     }));
+    server.use(createProxyMiddleware('/notification', {
+      target: process.env.API_PROXY || process.env.API_SOCKET_SERVER_NOTIFICATION || 'http://127.0.0.1:3002',
+      ws: true,
+      changeOrigin: true,
+      cookieDomainRewrite: ''
+    }));
+  }
 
-//     server.use(createProxyMiddleware('/notification', {
-//       target: process.env.API_PROXY || process.env.API_SOCKET_SERVER_NOTIFICATION || 'http://127.0.0.1:3002',
-//       ws: true,
-//       changeOrigin: true,
-//       cookieDomainRewrite: ''
-//     }));
-//   }
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
 
-//   server.all('*', (req, res) => {
-//     return handle(req, res);
-//   });
-
-//   server.listen(port, () => {
-//     console.log(`> Ready on http://localhost:${port}`);
-//   });
-// });
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
 
