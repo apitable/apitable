5680ebfb4b4bca8cbea33a75db73c3b81a1fb26b
Guofeng Chen <66124783+functionChenGuoFeng@users.noreply.github.com>
2023-08-25 16:47:02 +0800

    chore(backend-server): migration resources ro parameters deserialize (#7060)
    <!-- 
    # What?
     Migrate attachment resource interfaces and add custom deserialization
    -->
    
    
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    - New Feature: Added a custom deserializer class
    `StringOrArrayToStringArrayDeserializer` to handle JSON values that can
    be either a string or an array of strings. This allows for more flexible
    parsing of JSON data.
    - New Feature: Updated the `MigrationResourcesRo` class to use the
    custom deserializer for the `resourceKeys` field, enabling conversion of
    a string or an array of strings to a list of strings during JSON
    deserialization. This improves the handling of resource keys in the
    application.
    
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

---
+++ backend-server/shared/core/src/main/java/com/apitable/core/support/deserializer/StringOrArrayToStringArrayDeserializer.java
@@ -0,0 +1,35 @@
+package com.apitable.core.support.deserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StringOrArrayToStringArrayDeserializer extends JsonDeserializer<List<String>> {
+    @Override
+    public List<String> deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        if (jsonParser.currentToken() == JsonToken.VALUE_STRING) {
+            // If it's a string value, process it
+            String rawValue = jsonParser.getValueAsString();
+            // Assuming keys are comma-separated
+            String[] keyArray = rawValue.split(",");
+
+            return Arrays.asList(keyArray);
+        } else if (jsonParser.currentToken() == JsonToken.START_ARRAY) {
+            // If it's a JSON array, process it accordingly
+            List<String> keysList = new ArrayList<>();
+            while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+                String key = jsonParser.getText();
+                keysList.add(key);
+            }
+            return keysList;
+        } else {
+            throw new IOException("Unexpected JSON format");
+        }
+    }
+}
