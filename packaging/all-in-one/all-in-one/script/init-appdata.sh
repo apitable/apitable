3a97190745d9331fdb27a24bc0793fb52599661b
vac (Brendan) <wangjintao@apitable.com>
2023-09-14 15:42:29 +0800

    chore: update liquibase and databus in all-in-one (#7531)

--- packaging/all-in-one/all-in-one/script/init-appdata.sh
+++ packaging/all-in-one/all-in-one/script/init-appdata.sh
@@ -2,21 +2,32 @@
 
 set -e
 
-for action in releaseLocks update; do
-    liquibase \
-        "--defaultsFile=/liquibase/liquibase.docker.properties" \
-        --classpath=/liquibase/changelog \
-        --changelog-file="${CHANGELOG_FILE:=db/changelog/db.changelog-master.xml}" \
-        --log-level=warning \
-        --username="${MYSQL_USERNAME}" \
-        --password="${MYSQL_PASSWORD}" \
-        --driver="com.mysql.cj.jdbc.Driver" \
-        --database-changelog-table-name="${DATABASE_TABLE_PREFIX:=apitable_}db_changelog" \
-        --database-changelog-lock-table-name="${DATABASE_TABLE_PREFIX:=apitable_}db_changelog_lock" \
-        --url="jdbc:mysql://${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}?characterEncoding=utf8&autoReconnect=true&useSSL=true" \
-        "${action}" \
-        -Dtable.prefix="${DATABASE_TABLE_PREFIX:=apitable_}"
-done
+liquibase \
+    "--defaultsFile=/liquibase/liquibase.docker.properties" \
+    --classpath=/liquibase/changelog \
+    --changelog-file="${CHANGELOG_FILE:=db/changelog/db.changelog-master.xml}" \
+    --log-level=warning \
+    --username="${MYSQL_USERNAME}" \
+    --password="${MYSQL_PASSWORD}" \
+    --driver="com.mysql.cj.jdbc.Driver" \
+    --database-changelog-table-name="${DATABASE_TABLE_PREFIX:=apitable_}db_changelog" \
+    --database-changelog-lock-table-name="${DATABASE_TABLE_PREFIX:=apitable_}db_changelog_lock" \
+    --url="jdbc:mysql://${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}?characterEncoding=utf8&autoReconnect=true&useSSL=true" \
+    clearChecksums
+
+liquibase \
+    "--defaultsFile=/liquibase/liquibase.docker.properties" \
+    --classpath=/liquibase/changelog \
+    --changelog-file="${CHANGELOG_FILE:=db/changelog/db.changelog-master.xml}" \
+    --log-level=warning \
+    --username="${MYSQL_USERNAME}" \
+    --password="${MYSQL_PASSWORD}" \
+    --driver="com.mysql.cj.jdbc.Driver" \
+    --database-changelog-table-name="${DATABASE_TABLE_PREFIX:=apitable_}db_changelog" \
+    --database-changelog-lock-table-name="${DATABASE_TABLE_PREFIX:=apitable_}db_changelog_lock" \
+    --url="jdbc:mysql://${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}?characterEncoding=utf8&autoReconnect=true&useSSL=true" \
+    update \
+    -Dtable.prefix="${DATABASE_TABLE_PREFIX:=apitable_}"
 
 for action in init-user load; do
     java -jar /app/init-appdata/init-appdata.jar "${action}"
