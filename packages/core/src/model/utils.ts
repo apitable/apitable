f5cdbf15d79edd5099de9c6f582f03f5230136bd
ziqiang <zhaozhiqiang@vikadata.com>
2023-09-26 10:16:13 +0800

    fix: field map support two string mapper (#7746)
    # Human says
    
    As a human, what have you done for this pull request?
    Why? What? How?
    <!-- 如果这个Pull Request有对应的Issue，你可以复制Issue的相关信息。 -->
    <!-- 
    # Why?
    > 对应哪个issue?
    > 为什么要这个pull request？
    > 背景故事或原因是怎样的？
    > 你理解的业务需求是怎样的？ 
    -->
    
    <!-- 
    # What?
    > 这是一个什么Pull Request？
    > 描述一下是什么？
    > 对哪些人有好处？ 
    -->
    
    <!-- 
    # How?
    > 大概描述下，如何具体实现的这个Pull Request？ 
    -->
    
    
    # AI says
    
    <!-- AI auto review added here -->
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    - Refactor: Introduced `ReversedFieldTypeStringMap` for reverse mapping
    of field types in the `utils.ts` file, improving code flexibility and
    maintainability.
    - Refactor: Modified `getFieldTypeByString` function to utilize
    `ReversedFieldTypeStringMap` for efficient reverse lookup.
    - No direct impact on end-users, but these changes enhance the internal
    logic and organization of the codebase.
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

--- packages/core/src/model/utils.ts
+++ packages/core/src/model/utils.ts
@@ -178,12 +178,43 @@ const FieldTypeStringMap = {
   [FieldType.OneWayLink]: APIMetaFieldType.OneWayLink,
 };
 
+const ReversedFieldTypeStringMap = {
+  [APIMetaFieldType.Attachment]: FieldType.Attachment,
+  [APIMetaFieldType.AutoNumber]: FieldType.AutoNumber,
+  [APIMetaFieldType.Checkbox]: FieldType.Checkbox,
+  [APIMetaFieldType.CreatedBy]: FieldType.CreatedBy,
+  [APIMetaFieldType.CreatedTime]: FieldType.CreatedTime,
+  [APIMetaFieldType.Currency]: FieldType.Currency,
+  [APIMetaFieldType.DateTime]: FieldType.DateTime,
+  [APIMetaFieldType.Email]: FieldType.Email,
+  [APIMetaFieldType.Formula]: FieldType.Formula,
+  [APIMetaFieldType.LastModifiedBy]: FieldType.LastModifiedBy,
+  [APIMetaFieldType.LastModifiedTime]: FieldType.LastModifiedTime,
+  [APIMetaFieldType.MagicLink]: FieldType.Link,
+  [APIMetaFieldType.TwoWayLink]: FieldType.Link,
+  [APIMetaFieldType.MagicLookUp]: FieldType.LookUp,
+  [APIMetaFieldType.Member]: FieldType.Member,
+  [APIMetaFieldType.MultiSelect]: FieldType.MultiSelect,
+  [APIMetaFieldType.NotSupport]: FieldType.NotSupport,
+  [APIMetaFieldType.Number]: FieldType.Number,
+  [APIMetaFieldType.Percent]: FieldType.Percent,
+  [APIMetaFieldType.Phone]: FieldType.Phone,
+  [APIMetaFieldType.Rating]: FieldType.Rating,
+  [APIMetaFieldType.SingleSelect]: FieldType.SingleSelect,
+  [APIMetaFieldType.SingleText]: FieldType.SingleText,
+  [APIMetaFieldType.Text]: FieldType.Text,
+  [APIMetaFieldType.URL]: FieldType.URL,
+  [APIMetaFieldType.Cascader]: FieldType.Cascader,
+  [APIMetaFieldType.OneWayLink]: FieldType.OneWayLink,
+};
+
+
 export const getFieldTypeString = (fieldType: FieldType): APIMetaFieldType => {
   return FieldTypeStringMap[fieldType];
 };
 
 export const getFieldTypeByString = (fieldType: APIMetaFieldType): FieldType | undefined => {
-  return Number(Object.keys(FieldTypeStringMap).find(key => FieldTypeStringMap[key] === fieldType));
+  return ReversedFieldTypeStringMap[fieldType];
 };
 
 const MemberTypeStringMap = {
