2e96914465e1eeabbdfeb17e6e9fc3868c1a09bf
Robot Ye Chen <262000730@qq.com>
2023-10-08 20:16:40 +0800

    feat: datasheet action impl in rust (#7412)
    site目录测试通过
    ```
    cd databus-wasm
    make build
    cd site
    yarn
    yarn test
    ```
    验收一种测试条件
    - [x] addView2Action
    - [x] moveView2Action
    - [x] deleteView2Action
    - [x] modifyView2Action
    - [x] setColumnWidth2Action
    - [x] addField2Action
    - [x] delField2Action
    
    # Human says
    
    As a human, what have you done for this pull request?
    Why? What? How?
    <!-- 如果这个Pull Request有对应的Issue，你可以复制Issue的相关信息。 -->
    <!-- 
    # Why?
    > 对应哪个issue?
    > 为什么要这个pull request？
    > 背景故事或原因是怎样的？
    > 你理解的业务需求是怎样的？ 
    -->
    
    <!-- 
    # What?
    > 这是一个什么Pull Request？
    > 描述一下是什么？
    > 对哪些人有好处？ 
    -->
    
    <!-- 
    # How?
    > 大概描述下，如何具体实现的这个Pull Request？ 
    -->
    
    
    # AI says
    
    <!-- AI auto review added here -->
    
    
    
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    Release Notes:
    
    - New Feature: Added new test cases for "add field" and
    `deleteField2Action` functionalities in the `DatasheetActions` module.
    - New Feature: Introduced new modules/files `MockDataForAddFieldAction`
    and `MockDataForDelFieldAction` in Rust that export constant objects
    containing mock data for testing purposes.
    - Bug Fix: Fixed import statements and introduced new functions related
    to datasheet actions in Rust code.
    - Refactor: Modified `ViewColumnSO` and `ViewRowSO` structs, and made
    certain fields public in `sort.rs`.
    - Chore: Added an additional import statement for the `uuid` crate in
    `action.rs`.
    - Test: Added a new test case for modifying a view and introduced mock
    data for an action in a datasheet.
    - New Feature: Implemented new functions for adding, moving, deleting,
    and modifying views, as well as adding and deleting fields in Rust code.
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->
    
    ---------
    
    Co-authored-by: yechen <18412022+SmileChen518@users.noreply.github.com>

---
+++ packages/core/src/commands_actions/__test__/mock_data/action_add_field_mock.ts
@@ -0,0 +1,107 @@
+const MockDataForAddFieldAction = {
+  meta: {
+    views: [
+      {
+        id: 'viwuTxAXZp9fR',
+        name: '维格视图',
+        rows: [
+          {
+            recordId: 'recYYHEw1Z9rY'
+          },
+          {
+            recordId: 'recjbJjmxPU3P'
+          },
+          {
+            recordId: 'recRSbPCgeZZW'
+          }
+        ],
+        type: 1,
+        columns: [
+          {
+            fieldId: 'fld9NDJEvLCyl',
+            statType: 1
+          },
+          {
+            fieldId: 'fld1ThrVEcDMT'
+          },
+          {
+            fieldId: 'fldZwKsldHHNg'
+          }
+        ],
+        autoSave: false,
+        frozenColumnCount: 1,
+        displayHiddenColumnWithinMirror: false
+      }
+    ],
+    fieldMap: {
+      fld1ThrVEcDMT: {
+        id: 'fld1ThrVEcDMT',
+        name: '选项',
+        type: 4,
+        property: {
+          options: []
+        }
+      },
+      fld9NDJEvLCyl: {
+        id: 'fld9NDJEvLCyl',
+        name: '标题',
+        type: 19,
+        property: {
+          defaultValue: ''
+        }
+      },
+      fldZwKsldHHNg: {
+        id: 'fldZwKsldHHNg',
+        name: '附件',
+        type: 6
+      }
+    }
+  },
+  recordMap: {
+    recjbJjmxPU3P: {
+      id: 'recjbJjmxPU3P',
+      data: {},
+      createdAt: 1692671333000,
+      updatedAt: 1692671333000,
+      revisionHistory: [
+        0
+      ],
+      recordMeta: {
+        createdAt: 1692671333414,
+        createdBy: '0a6622aa1f40437ab1fa2ed30afc1ee4'
+      },
+      commentCount: 0
+    },
+    recRSbPCgeZZW: {
+      id: 'recRSbPCgeZZW',
+      data: {},
+      createdAt: 1692671333000,
+      updatedAt: 1692671333000,
+      revisionHistory: [
+        0
+      ],
+      recordMeta: {
+        createdAt: 1692671333414,
+        createdBy: '0a6622aa1f40437ab1fa2ed30afc1ee4'
+      },
+      commentCount: 0
+    },
+    recYYHEw1Z9rY: {
+      id: 'recYYHEw1Z9rY',
+      data: {},
+      createdAt: 1692671333000,
+      updatedAt: 1692671333000,
+      revisionHistory: [
+        0
+      ],
+      recordMeta: {
+        createdAt: 1692671333414,
+        createdBy: '0a6622aa1f40437ab1fa2ed30afc1ee4'
+      },
+      commentCount: 0
+    }
+  },
+  datasheetId: 'dstckW8kzoZiUDMR76'
+};
+
+export default MockDataForAddFieldAction;
