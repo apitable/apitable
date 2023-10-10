94d321fd6b029ddede33d53442390259f1596c32
William Chan <root@williamchan.me>
2023-10-10 04:50:57 -0500

    refactor: The codebase has been refactored to remove unused AI-related functionality, ... (#8025)
    # Human says
    
    As a human, what have you done for this pull request?
    Why? What? How?
    <!-- 如果这个Pull Request有对应的Issue，你可以复制Issue的相关信息。 -->
    <!-- 
    # Why?
    > 对应哪个issue?
    > 为什么要这个pull request？
    > 背景故事或原因是怎样的？
    > 你理解的业务需求是怎样的？ 
    -->
    
    <!-- 
    # What?
    > 这是一个什么Pull Request？
    > 描述一下是什么？
    > 对哪些人有好处？ 
    -->
    
    <!-- 
    # How?
    > 大概描述下，如何具体实现的这个Pull Request？ 
    -->
    
    
    # AI says
    
    <!-- AI auto review added here -->
    
    
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    - Refactor: The codebase has been refactored to remove unused AI-related
    functionality, including API endpoints, enums, interfaces, and utility
    functions. Import statements have been updated to use the `Shared`
    module from `'enterprise/chat/shared'` instead of `'@apitable/core'`.
    This refactoring improves code maintainability and removes unnecessary
    dependencies.
    - Bug Fix: Fixed a missing return statement in the `Message` component
    of the chat list, ensuring proper function behavior.
    - Documentation: No changes related to documentation were identified in
    the provided summary.
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

--- packages/core/src/exports/api/index.ts
+++ packages/core/src/exports/api/index.ts
@@ -17,7 +17,7 @@
  */
 
 // core/api is a compatible layer after modularize
-
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import * as Api from '../../modules/shared/api/api';
 import * as Url from '../../modules/shared/api/url';
 import * as DatasheetApi from '../../modules/database/api/datasheet_api';
@@ -31,3 +31,69 @@ export * as DashboardApi from '../../modules/database/api/dashboard_api';
 export * from '../../modules/database/api/datasheet_api.interface';
 
 export { Url, Api, DatasheetApi, IApi, FormApi, WidgetApi, ApiInterface, WidgetApiInterface };
+
+export interface IServerResponse<T = any> {
+  success: boolean;
+  message: string;
+  code: number;
+  data: T;
+}
+
+const baseURL = Url.BASE_URL;
+// const nestURL = Url.NEST_BASE_URL;
+
+class ServerResponseError extends Error {
+  public code!: number;
+  constructor(data: AxiosResponse<IServerResponse>) {
+    typeof data.data === 'object' ? super(data.data.message) : super(data.data);
+
+    this.name = 'ServerResponseError';
+    if (typeof data.data === 'object') {
+      this.code = data.data.code;
+      console.error(`request: ${data.config.url}\nmessage: ${data.data.message}`, data.data);
+    } else {
+      this.code = data.status;
+      console.error(`request: ${data.config.url}\nmessage: ${data.data}`);
+    }
+  }
+}
+
+const onResponseError = (error: AxiosError<IServerResponse>) => {
+  if (error.response) {
+    if (error.isAxiosError) {
+      const { response } = error;
+      if (response) {
+        const { data } = response;
+        const { message } = data;
+        if (message) {
+          error.message = message;
+        }
+      }
+    }
+    throw new ServerResponseError(error.response!);
+  } else {
+    // offline or timeout ?
+    throw error;
+  }
+};
+
+const onResponse = (response: AxiosResponse<IServerResponse>) => {
+  if (response.data.success === false) {
+    throw new ServerResponseError(response);
+  } else {
+    return response.data;
+  }
+
+};
+const createAxios = (config: AxiosRequestConfig) => {
+  const http = axios.create(Object.assign({
+    timeout: 20 * 1000,
+    withCredentials: true,
+  }, config));
+  http.interceptors.response.use(onResponse, onResponseError);
+  return http;
+};
+
+export const http = createAxios({
+  baseURL,
+});
