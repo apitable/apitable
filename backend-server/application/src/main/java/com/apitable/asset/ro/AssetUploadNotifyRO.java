8f04112500188dc540e7b2fe90b2c18f90951266
Chambers <chenbochao@vikadata.com>
2023-10-16 15:57:51 +0800

    feat(backend-server): add document asset type (#8120)
    @coderabbitai: ignore

--- backend-server/application/src/main/java/com/apitable/asset/ro/AssetUploadNotifyRO.java
+++ backend-server/application/src/main/java/com/apitable/asset/ro/AssetUploadNotifyRO.java
@@ -19,6 +19,7 @@
 package com.apitable.asset.ro;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import java.util.List;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
@@ -30,13 +31,14 @@ import lombok.Data;
 @Schema(description = "Resource upload completion notification RO")
 public class AssetUploadNotifyRO {
 
-    @Schema(description = "Type (0: user avatar; 1: space logo; 2: number table attachment; 3: "
-        + "cover image; 4: node description)", example = "0", required = true)
+    @Schema(description = "Asset Type(0:user avatar;1:space logo;2:datasheet;"
+        + " 3:cover image;4:node description;5:document)",
+        example = "0", requiredMode = RequiredMode.REQUIRED)
     @NotNull(message = "Type cannot be null")
     private Integer type;
 
-    @Schema(description = "List of resource names", example = "[\"spc10/2019/12/10/159\", "
-        + "\"spc10/2019/12/10/168\"]")
+    @Schema(description = "List of resource names",
+        example = "[\"spc10/2019/12/10/159\", \"spc10/2019/12/10/168\"]")
     private List<String> resourceKeys;
 
 }
