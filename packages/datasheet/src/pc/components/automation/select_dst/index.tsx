367c017f1db30162616d6a919ca43dad1d81e63c
Caedman Ziwen Lan <lanziwen@apitable.com>
2023-10-20 14:36:40 +0800

    fix: fix datasheet select (#8285)
    close https://github.com/vikadata/vikadata/issues/8059

--- packages/datasheet/src/pc/components/automation/select_dst/index.tsx
+++ packages/datasheet/src/pc/components/automation/select_dst/index.tsx
@@ -1,14 +1,21 @@
 import { useAtomValue } from 'jotai';
 import { FC, memo, useState } from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
-import { ConfigConstant, IReduxState, Selectors, Strings, t } from '@apitable/core';
-import { DataSourceSelectorForNode } from 'pc/components/data_source_selector_enhanced/data_source_selector_for_node/data_source_selector_for_node';
+import useSWR from 'swr';
+import { ConfigConstant, INode, IReduxState, Selectors, Strings, t } from '@apitable/core';
+import { getNodeInfo } from '@apitable/core/dist/modules/space/api/api.space';
+import {
+  DataSourceSelectorForNode
+} from 'pc/components/data_source_selector_enhanced/data_source_selector_for_node/data_source_selector_for_node';
 import { SearchPanel } from 'pc/components/datasheet_search_panel';
 import { RelatedResource } from '../../robot/robot_context';
 import { automationStateAtom, loadableFormItemAtom } from '../controller';
 import { SelectTrigger } from './select_trigger';
 
-export const SelectDst: FC<{ value: string; onChange: (dstId: string | undefined) => void }> = memo(({ value, onChange }) => {
+export const SelectDst: FC<{ value: string; onChange: (dstId: string | undefined) => void }> = memo(({
+  value,
+  onChange
+}) => {
   const datasheet = useSelector((a) => Selectors.getDatasheet(a, value), shallowEqual);
   const { rootId } = useSelector((state: IReduxState) => state.catalogTree);
 
@@ -17,7 +24,7 @@ export const SelectDst: FC<{ value: string; onChange: (dstId: string | undefined
   const stateValue = useAtomValue(automationStateAtom);
   const { shareId } = useSelector((state: IReduxState) => state.pageParams);
   const name =
-    shareId != null ? stateValue?.robot?.relatedResources?.find((item: RelatedResource) => item.nodeId === value)?.nodeName : datasheet?.name;
+        shareId != null ? stateValue?.robot?.relatedResources?.find((item: RelatedResource) => item.nodeId === value)?.nodeName : datasheet?.name;
   return (
     <>
       <SelectTrigger
@@ -65,7 +72,21 @@ export const SelectDst: FC<{ value: string; onChange: (dstId: string | undefined
   );
 });
 
-export const SelectForm: FC<{ value: string; onChange: (dstId: string | undefined) => void }> = memo(({ value, onChange }) => {
+export const useFolderId = (formId: string) => {
+
+  const { isLoading, data } = useSWR(['getNodeInfo', formId], () => getNodeInfo(formId), {
+    isPaused: () => formId == null
+  });
+  return {
+    isLoading: isLoading,
+    data: data?.data?.data[0] as INode
+  };
+};
+
+export const SelectForm: FC<{ value: string; onChange: (dstId: string | undefined) => void }> = memo(({
+  value,
+  onChange
+}) => {
   const { rootId } = useSelector((state: IReduxState) => state.catalogTree);
   const { shareId } = useSelector((state: IReduxState) => state.pageParams);
   const treeMaps = useSelector((state: IReduxState) => state.catalogTree.treeNodesMap);
@@ -79,12 +100,14 @@ export const SelectForm: FC<{ value: string; onChange: (dstId: string | undefine
   const node = treeMaps[value];
 
   let name =
-    shareId != null
-      ? stateValue?.robot?.relatedResources?.find((item: RelatedResource) => item.nodeId === value)?.nodeName
-      : (formMeta?.data as any)?.form?.name ?? node?.nodeName;
-  if(form) {
+        shareId != null
+          ? stateValue?.robot?.relatedResources?.find((item: RelatedResource) => item.nodeId === value)?.nodeName
+          : (formMeta?.data as any)?.form?.name ?? node?.nodeName;
+  if (form) {
     name = form.name;
   }
+
+  const { isLoading, data } = useFolderId(value);
 
   return (
     <>
@@ -98,20 +121,49 @@ export const SelectForm: FC<{ value: string; onChange: (dstId: string | undefine
         label={name}
       />
       {visible && (
-        <DataSourceSelectorForNode
-          onHide={() => setVisible(false)}
-          permissionRequired={'manageable'}
-          onChange={({ formId }) => {
-            if (value === formId) {
-              return;
-            }
-            onChange(formId);
-            setVisible(false);
-          }}
-          nodeTypes={[ConfigConstant.NodeType.FORM, ConfigConstant.NodeType.FOLDER]}
-          defaultNodeIds={{ datasheetId: value, folderId: rootId, formId: value }}
-          requiredData={['formId']}
-        />
+        <>
+          {
+            value == null ? (
+              <DataSourceSelectorForNode
+                onHide={() => setVisible(false)}
+                permissionRequired={'manageable'}
+                onChange={({ formId }) => {
+                  if (value === formId) {
+                    return;
+                  }
+                  onChange(formId);
+                  setVisible(false);
+                }}
+                nodeTypes={[ConfigConstant.NodeType.FORM, ConfigConstant.NodeType.FOLDER]}
+                defaultNodeIds={{ folderId: data?.parentId ?? rootId, formId: value }}
+                requiredData={['formId']}
+              />
+            ) : (
+              <>{
+                !isLoading && (
+                  <DataSourceSelectorForNode
+                    onHide={() => setVisible(false)}
+                    permissionRequired={'manageable'}
+                    onChange={({ formId }) => {
+                      if (value === formId) {
+                        return;
+                      }
+                      onChange(formId);
+                      setVisible(false);
+                    }}
+                    nodeTypes={[ConfigConstant.NodeType.FORM, ConfigConstant.NodeType.FOLDER]}
+                    defaultNodeIds={{
+                      folderId: (data?.parentId) ? data?.parentId : rootId,
+                      formId: value
+                    }}
+                    requiredData={['formId']}
+                  />
+
+                )
+              }</>
+            )
+          }
+        </>
       )}
     </>
   );
