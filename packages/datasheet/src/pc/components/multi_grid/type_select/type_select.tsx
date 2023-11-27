2105df5eafe0f495ca83c389d0111e96d0461e89
Kilian <wangkailang@users.noreply.github.com>
2023-11-27 23:29:11 +0800

    fix: product ce crash (#1485)
    Submit a pull request for this project.
    
    <!-- If you have an Issue that related to this Pull Request, you can
    copy this Issue's description -->
    
    # Why? 
    <!-- 
    > Related to which issue?
    > Why we need this pull request?
    > What is the user story for this pull request? 
    -->
    
    
    # What?
    <!-- 
    > Can you describe this feature in detail?
    > Who can benefit from it? 
    -->
    
    
    # How?
    <!-- 
    > Do you have a simple description of how this pull request is
    implemented?
    -->

--- packages/datasheet/src/pc/components/multi_grid/type_select/type_select.tsx
+++ packages/datasheet/src/pc/components/multi_grid/type_select/type_select.tsx
@@ -198,8 +198,10 @@ export const TypeSelectBase: React.FC<React.PropsWithChildren<ITypeSelect>> = (p
   });
 
   function filterPrimaryType(fieldType: FieldType) {
+    if (fieldType === FieldType.WorkDoc) {
+      return IS_ENTERPRISE && getEnvVariables().ENABLE_WORKDOC_FIELD;
+    }
     if (props.fieldIndex !== 0) return true;
-    if (IS_ENTERPRISE && fieldType === FieldType.WorkDoc && getEnvVariables().ENABLE_WORKDOC_FIELD) return true;
     return FieldTypeDescriptionMap[fieldType] && FieldTypeDescriptionMap[fieldType].canBePrimaryField;
   }
 
