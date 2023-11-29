107d096c737f5fec50c6d866160f8fff7c792061
Chambers <chenbochao@vikadata.com>
2023-11-29 19:39:17 +0800

    chore: replace node cte query method (#9347)

--- packages/room-server/src/node/repositories/node.repository.ts
+++ packages/room-server/src/node/repositories/node.repository.ts
@@ -63,69 +63,6 @@ export class NodeRepository extends Repository<NodeEntity> {
       .getRawOne<{ spaceId: string }>();
   }
 
-  /**
-   * Obtain the children node list of a given node
-   */
-  public async selectAllSubNodeIds(nodeId: string): Promise<string[]> {
-    // todo(itou): replace dynamic sql
-    const raws = await this.query(
-      `
-          WITH RECURSIVE sub_ids (node_id) AS
-          (
-            SELECT node_id
-            FROM ${this.manager.connection.options.entityPrefix}node
-            WHERE parent_id = ? and is_rubbish = 0
-            UNION ALL
-            SELECT c.node_id
-            FROM sub_ids AS cp
-            JOIN ${this.manager.connection.options.entityPrefix}node AS c ON cp.node_id = c.parent_id and c.is_rubbish = 0
-          )
-          SELECT distinct node_id nodeId
-          FROM sub_ids;
-        `,
-      [nodeId],
-    );
-    return raws.reduce((pre: string[], cur: { nodeId: string }) => {
-      pre.push(cur.nodeId);
-      return pre;
-    }, []);
-  }
-
-  /**
-   * Obtain the path to the root node of a given node.
-   *
-   * The returned node ID array includes the given node and does not include the root node.
-   *
-   * Example: for a path of 3 nodes, the returned array is `[nodeId, parentId, grandparentId, great-grandparentId]`
-   */
-  async selectParentPathByNodeId(nodeId: string): Promise<string[]> {
-    // todo(itou): replace dynamic sql
-    // Query the path with recursive SQL, the result set includes the given node.
-    const raws = await this.query(
-      `
-          WITH RECURSIVE parent_view (node_id, node_name, parent_id, lvl) AS
-          (
-            SELECT n.node_id, n.node_name, n.parent_id, 0 lvl
-            FROM ${this.manager.connection.options.entityPrefix}node n
-            WHERE n.node_id = ? AND n.is_rubbish = 0
-            UNION ALL
-            SELECT c.node_id, c.node_name, c.parent_id, pv.lvl + 1
-            FROM parent_view AS pv
-            JOIN ${this.manager.connection.options.entityPrefix}node AS c ON pv.parent_id = c.node_id AND c.is_rubbish = 0
-          )
-          SELECT node_id nodeId
-          FROM parent_view
-          WHERE parent_id != '0'
-          ORDER BY lvl ASC
-        `,
-      [nodeId],
-    );
-    return raws.reduce((pre: string[], cur: { nodeId: string }) => {
-      pre.push(cur.nodeId);
-      return pre;
-    }, []);
-  }
-
   public async getNodeInfo(nodeId: string): Promise<NodeEntity | undefined> {
     return await this.findOne({
       select: ['nodeId', 'nodeName', 'spaceId', 'parentId', 'icon', 'extra', 'type'],
