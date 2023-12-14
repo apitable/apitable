f4f7ec1f3d3e08a115f5d67348fe365cdaf4e9ca
Chambers <chenbochao@vikadata.com>
2023-12-14 10:25:55 +0800

    chore: tune ops controller & exposed code (#9800)

--- backend-server/application/src/main/java/com/apitable/asset/vo/AssetUrlSignatureVo.java
+++ backend-server/application/src/main/java/com/apitable/asset/vo/AssetUrlSignatureVo.java
@@ -14,9 +14,10 @@ import lombok.NoArgsConstructor;
 @Schema(description = "Attachment resource url add signature")
 public class AssetUrlSignatureVo {
     @Schema(description = "File Access Path(possibly non-final value)",
-        example = "/space/2023/03/07/8195212453744848953f87c54dc0369b")
+        example = "space/2023/03/07/8195212453744848953f87c54dc0369b")
     private String resourceKey;
     @Schema(description = "Path after signature",
-        example = "https://s1-test.vika.ltd//space/2023/03/07/8195212453744848953f87c54dc0369b?sign=6bede194fb458c876676139f469b5576&t=64d4bb25")
+        example = "https://aitable.ai/space/2023/03/07/8195212453744848953f87c54dc0369b"
+            + "?sign=6bede194fb458c876676139f469b5576&t=64d4bb25")
     private String url;
 }
--- backend-server/application/src/main/java/com/apitable/automation/service/impl/AutomationTriggerTypeServiceImpl.java
+++ backend-server/application/src/main/java/com/apitable/automation/service/impl/AutomationTriggerTypeServiceImpl.java
@@ -18,22 +18,9 @@
 
 package com.apitable.automation.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.util.StrUtil;
-import com.apitable.automation.entity.AutomationTriggerTypeEntity;
 import com.apitable.automation.mapper.AutomationTriggerTypeMapper;
-import com.apitable.automation.model.TriggerTypeCreateRO;
-import com.apitable.automation.model.TriggerTypeEditRO;
-import com.apitable.automation.service.IAutomationService;
 import com.apitable.automation.service.IAutomationTriggerTypeService;
-import com.apitable.base.enums.DatabaseException;
-import com.apitable.core.exception.BusinessException;
-import com.apitable.core.util.ExceptionUtil;
-import com.apitable.shared.util.IdUtil;
-import com.baomidou.mybatisplus.core.toolkit.IdWorker;
-import com.baomidou.mybatisplus.extension.toolkit.SqlHelper;
 import jakarta.annotation.Resource;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -45,53 +32,10 @@ import org.springframework.stereotype.Service;
 public class AutomationTriggerTypeServiceImpl implements IAutomationTriggerTypeService {
 
     @Resource
-    private IAutomationService iAutomationService;
-
-    @Resource
     private AutomationTriggerTypeMapper triggerTypeMapper;
 
     @Override
     public String getTriggerTypeByEndpoint(String endpoint) {
         return triggerTypeMapper.getTriggerTypeByEndpoint(endpoint);
-    }
-
-    @Override
-    public String create(Long userId, TriggerTypeCreateRO ro) {
-        iAutomationService.checkServiceIfExist(ro.getServiceId());
-        String triggerTypeId = StrUtil.isNotBlank(ro.getTriggerTypeId())
-            ? ro.getTriggerTypeId() : IdUtil.createAutomationTriggerTypeId();
-        AutomationTriggerTypeEntity entity =
-            BeanUtil.copyProperties(ro, AutomationTriggerTypeEntity.class);
-        entity.setId(IdWorker.getId());
-        entity.setTriggerTypeId(triggerTypeId);
-        entity.setCreatedBy(userId);
-        entity.setUpdatedBy(userId);
-        boolean flag = SqlHelper.retBool(triggerTypeMapper.insert(entity));
-        ExceptionUtil.isTrue(flag, DatabaseException.INSERT_ERROR);
-        return triggerTypeId;
-    }
-
-    @Override
-    public void edit(Long userId, String triggerTypeId, TriggerTypeEditRO ro) {
-        Long id = this.getIdByTriggerTypeId(triggerTypeId);
-        AutomationTriggerTypeEntity entity =
-            BeanUtil.copyProperties(ro, AutomationTriggerTypeEntity.class);
-        entity.setId(id);
-        entity.setUpdatedBy(userId);
-        boolean flag = SqlHelper.retBool(triggerTypeMapper.updateById(entity));
-        ExceptionUtil.isTrue(flag, DatabaseException.EDIT_ERROR);
-    }
-
-    @Override
-    public void delete(Long userId, String triggerTypeId) {
-        Long id = this.getIdByTriggerTypeId(triggerTypeId);
-        boolean flag = SqlHelper.retBool(triggerTypeMapper.deleteById(id));
-        ExceptionUtil.isTrue(flag, DatabaseException.DELETE_ERROR);
-    }
-
-    private Long getIdByTriggerTypeId(String triggerTypeId) {
-        Long id = triggerTypeMapper.selectIdByTriggerTypeId(triggerTypeId);
-        return Optional.ofNullable(id)
-            .orElseThrow(() -> new BusinessException("Trigger Type not exist."));
     }
 }
