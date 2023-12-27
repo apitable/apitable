fff5f1f8bce5eab299fbb4d1912a95d57c6380e8
Kilian <wangkailang@users.noreply.github.com>
2023-12-27 18:03:26 +0800

    fix: form desc show more (#10178)
    - fix: #10008
    
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    - Style: Reduced the margin-bottom property of form field descriptions
    and added new CSS classes for show more/less buttons.
    - Refactor: Updated the import path for the Workdoc component in the
    Workdoc Editor.
    - New Feature: Implemented line truncation for field descriptions using
    the react-multiline-clamp library and added show more/less buttons to
    toggle between full and truncated descriptions.
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

--- packages/datasheet/src/pc/components/form_container/form_field_container/form_field_ui.tsx
+++ packages/datasheet/src/pc/components/form_container/form_field_container/form_field_ui.tsx
@@ -19,7 +19,9 @@
 import classnames from 'classnames';
 import isNumber from 'lodash/isNumber';
 import * as React from 'react';
-import { colorVars, IconButton, useContextMenu } from '@apitable/components';
+// @ts-ignore
+import Clamp from 'react-multiline-clamp';
+import {colorVars, IconButton, LinkButton, useContextMenu} from '@apitable/components';
 import { ConfigConstant, Selectors, Strings, t } from '@apitable/core';
 import { AddOutlined, MoreOutlined } from '@apitable/icons';
 // eslint-disable-next-line no-restricted-imports
@@ -27,14 +29,12 @@ import { Tooltip } from 'pc/components/common';
 import { useAppendField } from 'pc/components/expand_record/hooks/use_append_field';
 import { useEditDesc } from 'pc/components/expand_record/hooks/use_edit_desc';
 import { useEditField } from 'pc/components/expand_record/hooks/use_edit_field';
-
 import { FormContext } from 'pc/components/form_container/form_context';
-import { UrlDiscern } from 'pc/components/multi_grid/cell/cell_text/url_discern';
+import { useAppSelector } from 'pc/store/react-redux';
 import { isTouchDevice } from 'pc/utils';
-
 import styles from './style.module.less';
 
-import {useAppSelector} from "pc/store/react-redux";
+const DESC_MAX_LINES = 5;
 
 interface IFormFieldUIProps {
   index: number;
@@ -120,9 +120,26 @@ export const FormFieldUI: React.FC<React.PropsWithChildren<IFormFieldUIProps>> =
       </h4>
       {/* {errorMsg && <div className={styles.errorMsg}>{errorMsg}</div>} */}
       {!descIsEmpty && (
-        <pre className={styles.fieldDesc}>
-          <UrlDiscern value={desc} />
-        </pre>
+        <Clamp
+          lines={DESC_MAX_LINES}
+          maxLines={1000}
+          withToggle
+          withTooltip={false}
+          showMoreElement={({ toggle }: { toggle: (e: React.MouseEvent) => void }) => (
+            <LinkButton underline={false} onClick={toggle} className={styles.showMore}>
+              {t(Strings.see_more)}
+            </LinkButton>
+          )}
+          showLessElement={({ toggle }: { toggle: (e: React.MouseEvent) => void }) => (
+            <LinkButton underline={false} onClick={toggle} className={styles.showLess}>
+              {t(Strings.collapse)}
+            </LinkButton>
+          )}
+        >
+          <pre className={styles.fieldDesc}>
+            {desc}
+          </pre>
+        </Clamp>
       )}
       {children}
     </div>
