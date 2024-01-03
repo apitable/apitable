f8049e10ca4250a6e66e91c7e25f9951b6bc7adb
Kilian <wangkailang@users.noreply.github.com>
2024-01-03 18:30:35 +0800

    feat: workdoc plugin image preview (#10327)
    - fix: #9946
    
    
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    - Bug Fix: The `PreviewImage` component now correctly assigns the value
    of `file.token` to `src` when the `src` variable is empty and the
    `file.token` includes the string "http". This ensures that the
    `file.token` can be used as the image source when the `isSupportImage`
    function returns false.
    - Documentation: The interface `IPreviewTypeBase` has been modified to
    make several properties optional. This may affect the external interface
    and behavior of the code.
    - New Feature: The `Resizer` component in the workdoc plugin now
    includes an image preview feature. Clicking on the image expands it
    using the `expandWorkdocImage` function, allowing users to view the
    image in detail and delete it if needed.
    - Refactor: The `PreviewType` component has been modified to indicate
    that the `userInfo` and `previewUrl` props are expected to always have a
    value by adding non-null assertions (`!`). This removes the possibility
    of these props being `null` or `undefined`.
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

--- packages/datasheet/src/pc/components/preview_file/preview_type/preview_type.interface.ts
+++ packages/datasheet/src/pc/components/preview_file/preview_type/preview_type.interface.ts
@@ -22,11 +22,11 @@ import { ITransFormInfo } from '../preview_file.interface';
 export interface IPreviewTypeBase {
   file: IAttachmentValue;
   transformInfo: ITransFormInfo;
-  userInfo: IUserInfo | null;
+  userInfo?: IUserInfo | null;
   spaceId?: string;
-  onClose: () => void;
-  officePreviewEnable: boolean;
-  previewUrl: string | null;
+  onClose?: () => void;
+  officePreviewEnable?: boolean;
+  previewUrl?: string | null;
   setTransformInfo: (transformInfo: ITransFormInfo, immediately?: boolean) => void;
   disabledDownload: boolean;
 }
