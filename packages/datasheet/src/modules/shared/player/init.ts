f7c599160e74a2bf35f6e5993283c059c70dc357
yechen <262000730@qq.com>
2024-01-08 10:41:51 +0800

    fix: ci err

--- packages/datasheet/src/modules/shared/player/init.ts
+++ packages/datasheet/src/modules/shared/player/init.ts
@@ -22,7 +22,6 @@ import { startActions, TriggerCommands } from '../apphook/trigger_commands';
 import { isEventStateMatch, isRulesPassed, isTimeRulePassed } from './rules';
 // @ts-ignore
 import { getPlayerHooks, updatePlayerConfig } from 'enterprise/guide/utils';
-import { modifyWizardConfig } from 'pc/common/wizard';
 
 // const Triggers = SystemConfig.player.trigger;
 
