6e0120e434fb618247057dd06da4252b50770209
Kilian <wangkailang@users.noreply.github.com>
2024-01-19 16:54:41 +0800

    fix: public share style (#10562)
    - fix: #10387
    
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    - Bug Fix: Fixed the visual layout issue in the `DisabledShareFile`
    component by increasing the height of an image from 150 to 195 pixels.
    - Refactor: Improved code organization and removed unnecessary elements
    in the `public_share_invite_link.tsx` file. Also added a check for
    `spaceFeatures.fileSharable` to conditionally render a component based
    on its value.
    
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

--- packages/datasheet/src/pc/components/catalog/share_node/disabled_share_file/disabled_share_file.tsx
+++ packages/datasheet/src/pc/components/catalog/share_node/disabled_share_file/disabled_share_file.tsx
@@ -23,8 +23,7 @@ import { Typography, useThemeColors } from '@apitable/components';
 import { ConfigConstant, integrateCdnHost, IReduxState, Settings, Strings, t } from '@apitable/core';
 import { EyeOpenOutlined } from '@apitable/icons';
 import styles from 'pc/components/catalog/share_node/public_link/style.module.less';
-
-import {useAppSelector} from "pc/store/react-redux";
+import { useAppSelector } from 'pc/store/react-redux';
 
 export const DisabledShareFile = ({ style }: { style?: React.CSSProperties }) => {
   const spaceResource = useAppSelector((state: IReduxState) => state.spacePermissionManage.spaceResource);
@@ -32,7 +31,7 @@ export const DisabledShareFile = ({ style }: { style?: React.CSSProperties }) =>
   return (
     <div className={styles.disabledFileSharable} style={style}>
       <div style={{ marginBottom: 24 }}>
-        <Image src={integrateCdnHost(Settings.view_calendar_guide_no_permission.value)} alt="" width={260} height={150} />
+        <Image src={integrateCdnHost(Settings.view_calendar_guide_no_permission.value)} alt="" width={260} height={195} />
       </div>
       <Typography variant={'h7'}>{t(Strings.disabled_file_shared)}</Typography>
       <Typography variant={'body4'} className={styles.desc}>
