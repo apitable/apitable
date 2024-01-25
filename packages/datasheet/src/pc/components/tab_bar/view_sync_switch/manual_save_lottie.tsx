9c0290ac61ba3ed05d1bf78b55ba0f916b0529d8
Kilian <wangkailang@users.noreply.github.com>
2024-01-25 16:57:01 +0800

    fix: view save lottie (#10626)
    - fix: #10621

--- packages/datasheet/src/pc/components/tab_bar/view_sync_switch/manual_save_lottie.tsx
+++ packages/datasheet/src/pc/components/tab_bar/view_sync_switch/manual_save_lottie.tsx
@@ -16,27 +16,28 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import SyncJson from 'static/json/sync.json';
 
 const MANUAL_SAVE_SVG_ID = 'MANUAL_SAVE_SVG_ID';
 export const ManualSaveLottie = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
-    const handle = document.getElementById(MANUAL_SAVE_SVG_ID);
-    if (!handle) {
+    if (!ref.current) {
       return;
     }
     import('lottie-web/build/player/lottie_svg').then((module) => {
       const lottie = module.default;
       lottie.loadAnimation({
-        container: handle,
+        // @ts-ignore
+        container: ref.current,
         renderer: 'svg',
         loop: false,
         autoplay: true,
         animationData: SyncJson,
       });
     });
-  }, []);
+  }, [ref]);
 
-  return <div id={MANUAL_SAVE_SVG_ID} style={{ display: 'flex' }} />;
+  return <div ref={ref} id={MANUAL_SAVE_SVG_ID} style={{ display: 'flex' }} />;
 };
