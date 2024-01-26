e5fa1da32e7253a180d388d3b7696dfa46866787
Kilian <wangkailang@users.noreply.github.com>
2024-01-26 17:56:50 +0800

    fix: delete archive record clear invalid link record (#10644)
    - fix: #10629
    - fix: #10623
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    - New Feature: Improved data validation in the `cell_format_checker`
    module by filtering out invalid data for fields of type `OneWayLink`.
    - Bug Fix: Fixed an issue in the `OneWayLinkField` class where archived
    record IDs were not included in the filtered result.
    - Bug Fix: Updated the `deleteArchivedRecords` function to properly
    handle linked records and remove references to deleted archive records.
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

--- packages/core/src/commands/datasheet/delete_archived_record.ts
+++ packages/core/src/commands/datasheet/delete_archived_record.ts
@@ -1,13 +1,15 @@
 
-import { ICollaCommandDef, ExecuteResult } from 'command_manager';
+import { ICollaCommandDef, ExecuteResult, ILinkedActions } from 'command_manager';
 import { CollaCommandName } from 'commands/enum';
 import {
   getActiveDatasheetId,
   getSnapshot,
 } from 'modules/database/store/selectors/resource/datasheet/base';
 import { DatasheetActions } from 'commands_actions/datasheet';
-import { IJOTAction } from 'engine/ot';
-import { ResourceType } from 'types';
+import { IJOTAction, OTActionName } from 'engine/ot';
+import { FieldType, ResourceType } from 'types';
+import { getCellValue } from '../../modules/database/store/selectors/resource';
+import { isEqual } from 'lodash';
 
 export interface IDeleteArchivedRecordsOptions {
   cmd: CollaCommandName.DeleteArchivedRecords;
@@ -28,6 +30,46 @@ export const deleteArchivedRecords: ICollaCommandDef<IDeleteArchivedRecordsOptio
     }
 
     const actions: IJOTAction[] = [];
+    const linkedActions: ILinkedActions[] = [];
+
+    // when delete archive record, clear linked archived records
+    for (const fieldId in snapshot.meta.fieldMap) {
+      const field = snapshot.meta.fieldMap[fieldId]!;
+      const _linkedActions: IJOTAction[] = [];
+      if (field.type === FieldType.Link && field.property.brotherFieldId && field.property.foreignDatasheetId !== datasheetId) {
+        const linkedSnapshot = getSnapshot(state, field.property.foreignDatasheetId)!;
+        data.forEach(record => {
+          const recordId = record.id;
+          const value = record.data[fieldId] as string[] | null;
+          if (value) {
+            value.forEach(id => {
+              const oldValue = getCellValue(state, linkedSnapshot, id, field.property.brotherFieldId!) as string[] | null;
+              const value = oldValue ? oldValue.filter(v => v !== recordId) : null;
+              if (isEqual(value, oldValue)) {
+                return;
+              }
+              const path = ['recordMap', id, 'data', field.property.brotherFieldId!];
+              const samePathIndex = _linkedActions.findIndex(action => isEqual(action.p, path));
+              if (samePathIndex > -1) {
+                // @ts-ignore
+                _linkedActions[samePathIndex].oi = _linkedActions[samePathIndex].oi.filter(v => v !== recordId);
+              } else {
+                _linkedActions.push({
+                  n: OTActionName.ObjectReplace,
+                  p: path,
+                  oi: value,
+                  od: oldValue,
+                });
+              }
+            });
+          }
+        });
+        linkedActions.push({
+          datasheetId: field.property.foreignDatasheetId,
+          actions: _linkedActions,
+        });
+      }
+    }
 
     const unarchiveRecordsActions = DatasheetActions.deleteArchivedRecords2Action(snapshot, { recordsData: data });
 
@@ -42,6 +84,7 @@ export const deleteArchivedRecords: ICollaCommandDef<IDeleteArchivedRecordsOptio
       resourceId: datasheetId,
       resourceType: ResourceType.Datasheet,
       actions,
+      linkedActions,
     };
   }
 };
