603eff0e92d10fd4cc85a2f90219841d120a724f
Kilian <wangkailang@users.noreply.github.com>
2024-02-23 10:11:47 +0800

    Revert "fix: sort lookup other datasheet link data" (#10757)
    Reverts vikadata/vikadata#10746
    <!-- This is an auto-generated comment: release notes by OSS CodeRabbit
    -->
    ### Summary by CodeRabbit
    
    - Refactor: Simplified row filtering logic in LookUpField class for
    improved performance and removed redundant code, enhancing user
    experience.
    <!-- end of auto-generated comment: release notes by OSS CodeRabbit -->

--- packages/core/src/model/field/lookup_field.ts
+++ packages/core/src/model/field/lookup_field.ts
@@ -698,20 +698,10 @@ export class LookUpField extends ArrayValueField {
     if (!snapshot) {
       return [];
     }
-
-    let rows = snapshot?.meta?.views[0]?.rows!.filter((row) => recordIds.includes(row.recordId));
-
-    // If the view is not found, the recordMap is used to find the rows
-    if (isEmpty(rows)) {
-      rows = Object.keys(snapshot.recordMap)
-        .filter((recordId) => recordIds.includes(recordId))
-        .map(recordId => ({ recordId }) as IViewRow);
-    }
-
+    const rows = snapshot?.meta?.views[0]?.rows!.filter((row) => recordIds.includes(row.recordId));
     if (!rows) {
       return [];
     }
-
     return sortInfo ? sortRowsBySortInfo(this.state, rows, sortInfo.rules, snapshot) : rows;
   }
 
