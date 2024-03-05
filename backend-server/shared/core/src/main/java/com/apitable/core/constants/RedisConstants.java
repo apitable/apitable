d7b692d79e6e3693c31ed2b1b62f3a3494b1d571
Zoe <zhengxu@apitable.com>
2024-03-05 16:27:41 +0800

    fix: api usage statistics (#10838)
    @coderabbitai: ignore
    
    Co-authored-by: Zoe <13400804+zoe-icu@users.noreply.github.com>

--- backend-server/shared/core/src/main/java/com/apitable/core/constants/RedisConstants.java
+++ backend-server/shared/core/src/main/java/com/apitable/core/constants/RedisConstants.java
@@ -374,4 +374,14 @@ public class RedisConstants {
     public static String getSpaceApiUsageConcurrentKey() {
         return StrUtil.format(GENERAL_LOCKED, "space", "api_usage");
     }
+
+    /**
+     * get api usage min id.
+     *
+     * @param date every day of begin time
+     * @return key
+     */
+    public static String getApiUsageTableDayMindIdCacheKey(String date) {
+        return StrUtil.format(GENERAL_STATICS, "api-usage-min-id-day", date);
+    }
 }
